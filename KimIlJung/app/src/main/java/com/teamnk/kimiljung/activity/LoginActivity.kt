089829856017f5dbde51d47866da5153faa1a0a0
Tmdhoon2 <hks026naver@gmail.com>
2022-09-25 12:36:19 +0900

    feat :: implement intent logic

--- KimIlJung/app/src/main/java/com/teamnk/kimiljung/activity/LoginActivity.kt
+++ KimIlJung/app/src/main/java/com/teamnk/kimiljung/activity/LoginActivity.kt
@@ -2,11 +2,27 @@ package com.teamnk.kimiljung.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.teamnk.kimiljung.R
+import com.teamnk.kimiljung.databinding.ActivityLoginBinding
+import com.teamnk.kimiljung.utils.IntentUtil
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding: ActivityLoginBinding by lazy{
+        DataBindingUtil.setContentView(this, R.layout.activity_login)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding.btnLoginLogin.setOnClickListener {
+            // Todo Server Logic
+            IntentUtil.startIntentClearTop(this, MainActivity::class.java)
+        }
+
+        binding.tvLoginGoToRegister.setOnClickListener {
+            IntentUtil.startIntent(this, RegisterActivity::class.java)
+        }
     }
 }
