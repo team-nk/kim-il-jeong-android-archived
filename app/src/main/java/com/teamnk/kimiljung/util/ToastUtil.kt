047e150ea78bd7c62eaeaac4d96f181d8d46aa40
JunsuMacbook <krpjs0508@gmail.com>
2022-09-28 14:33:38 +0900

    feat :: implement toast util

---
+++ app/src/main/java/com/teamnk/kimiljung/util/ToastUtil.kt
@@ -0,0 +1,12 @@
+package com.teamnk.kimiljung.util
+
+import android.content.Context
+import android.widget.Toast
+
+fun showShortToast(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
+
+fun showLongToast(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+}
