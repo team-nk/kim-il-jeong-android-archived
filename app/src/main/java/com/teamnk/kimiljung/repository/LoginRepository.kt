f544f2797edbfd34dc91657562072734dd433f2d
유현명 <aahspringaa4@naver.com>
2022-09-30 11:28:32 +0900

    feat :: LoginRepository

--- app/src/main/java/com/teamnk/kimiljung/repository/LoginRepository.kt
+++ app/src/main/java/com/teamnk/kimiljung/repository/LoginRepository.kt
@@ -1,4 +1,12 @@
 package com.teamnk.kimiljung.repository
 
-class LoginRepository {
+import com.teamnk.kimiljung.data.LoginApi
+import com.teamnk.kimiljung.dto.LoginRequest
+import com.teamnk.kimiljung.dto.LoginResponse
+
+class LoginRepository(
+    private val loginApi: LoginApi
+) {
+    suspend fun login(loginRequest: LoginRequest): LoginResponse =
+        loginApi.login(loginRequest)
 }
