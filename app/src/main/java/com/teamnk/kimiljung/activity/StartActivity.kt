988e691aff5080527c85886098e4ded386106a1f
JunsuMacbook <krpjs0508@gmail.com>
2022-10-02 19:17:53 +0900

    refactor :: StartActivity binding

--- app/src/main/java/com/teamnk/kimiljung/activity/StartActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/activity/StartActivity.kt
@@ -5,13 +5,13 @@ import androidx.databinding.DataBindingUtil
 import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.base.BaseActivity
 import com.teamnk.kimiljung.databinding.ActivityStartBinding
+import com.teamnk.kimiljung.util.initializeBinding
 import com.teamnk.kimiljung.util.startIntent
 
 class StartActivity : BaseActivity<ActivityStartBinding>(R.layout.activity_start) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        binding.lifecycleOwner = this
+        initializeBinding(this, binding, this)
 
         initStartLoginText()
     }
