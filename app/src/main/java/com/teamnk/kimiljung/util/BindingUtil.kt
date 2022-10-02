5dc25582ec4985991c09b0a64a52a771995608f1
JunsuMacbook <krpjs0508@gmail.com>
2022-10-02 19:17:24 +0900

    refactor :: remove no-used import in BindingUtil

--- app/src/main/java/com/teamnk/kimiljung/util/BindingUtil.kt
+++ app/src/main/java/com/teamnk/kimiljung/util/BindingUtil.kt
@@ -1,7 +1,6 @@
 package com.teamnk.kimiljung.util
 
 import android.app.Activity
-import android.content.Context
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.LifecycleOwner
 
