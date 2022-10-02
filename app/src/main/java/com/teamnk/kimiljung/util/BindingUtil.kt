0c0f8ea695abff6682e6368692c2c0f92578f294
JunsuMacbook <krpjs0508@gmail.com>
2022-10-02 19:15:31 +0900

    feat :: implement BindingUtil

---
+++ app/src/main/java/com/teamnk/kimiljung/util/BindingUtil.kt
@@ -0,0 +1,11 @@
+package com.teamnk.kimiljung.util
+
+import android.app.Activity
+import android.content.Context
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.LifecycleOwner
+
+fun initializeBinding(activity: Activity, binding: ViewDataBinding, lifecycleOwner: LifecycleOwner) {
+    activity.setContentView(binding.root)
+    binding.lifecycleOwner = lifecycleOwner
+}
