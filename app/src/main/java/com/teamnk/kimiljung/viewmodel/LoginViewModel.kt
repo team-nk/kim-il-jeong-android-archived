46b4b54ad0be05553d693bf837d0da5cf41e831f
JunsuMacbook <krpjs0508@gmail.com>
2022-10-04 23:29:37 +0900

    .

--- app/src/main/java/com/teamnk/kimiljung/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/teamnk/kimiljung/viewmodel/LoginViewModel.kt
@@ -2,15 +2,16 @@ package com.teamnk.kimiljung.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.teamnk.kimiljung.dto.LoginRequest
 import com.teamnk.kimiljung.repository.auth.LoginRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class LoginViewModel(
     private val rp: LoginRepository
 ) : ViewModel() {
-
     private var success: MutableLiveData<Boolean> = MutableLiveData()
     private val failed: MutableLiveData<Boolean> = MutableLiveData()
 
@@ -23,6 +24,16 @@ class LoginViewModel(
             success
         }.onFailure {
             failed
+        }
+    }
+
+    suspend fun startLogin(id: String, password: String) {
+        viewModelScope.launch {
+            postLogin(
+                LoginRequest(
+                    id, password
+                )
+            )
         }
     }
 
