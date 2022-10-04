b138471438fa639a06fe774db68b45ed9dd8a7b4
JunsuMacbook <krpjs0508@gmail.com>
2022-10-04 23:06:40 +0900

    feat :: LoginActivity viewModel

--- app/src/main/java/com/teamnk/kimiljung/activity/auth/LoginActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/activity/auth/LoginActivity.kt
@@ -1,16 +1,24 @@
-package com.teamnk.kimiljung.activity
+package com.teamnk.kimiljung.activity.auth
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.teamnk.kimiljung.R
+import com.teamnk.kimiljung.activity.MainActivity
 import com.teamnk.kimiljung.base.BaseActivity
 import com.teamnk.kimiljung.databinding.ActivityLoginBinding
 import com.teamnk.kimiljung.util.startIntent
 import com.teamnk.kimiljung.util.startIntentClearTop
+import com.teamnk.kimiljung.viewmodel.LoginViewModel
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
 
+    private val viewModel: LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        viewModel
         initLoginButton()
         initGoToRegisterText()
     }
