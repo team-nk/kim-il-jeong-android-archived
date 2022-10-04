ba9cdb76dbc06fc584c7e4b5332804b3521eb9aa
JunsuMacbook <krpjs0508@gmail.com>
2022-10-04 15:28:12 +0900

    feat :: DialogUtil 4

--- app/src/main/java/com/teamnk/kimiljung/util/DialogUtil.kt
+++ app/src/main/java/com/teamnk/kimiljung/util/DialogUtil.kt
@@ -2,8 +2,11 @@ package com.teamnk.kimiljung.util
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.WindowManager
 import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.databinding.DialogAllSingleButtonBinding
 
@@ -15,17 +18,21 @@ fun showDialogWithSingleButton(context: Context, activity: Activity) {
         )
     }
 
-    val dialogBuilder = AlertDialog.Builder(context)
+    /*val dialogBuilder = AlertDialog.Builder(context, R.layout.dialog_all_single_button)
         .setView(binding.root)
         .setCancelable(false)
-        .create()
+        .create()*/
+
+    val dialog = Dialog(context)
+    dialog.setContentView(binding.root)
+    dialog.setCancelable(false)
+
+    dialog.show()
 
     // TODO redefine things with parameter
     binding.tvDialogSingleTitle.setText(R.string.dialog_register_success_title)
     binding.tvDialogSingleDescription.setText(R.string.dialog_register_success_description)
     binding.btnDialogAccept.setOnClickListener {
-        dialogBuilder.cancel()
+        dialog.cancel()
     }
-
-    dialogBuilder.show()
 }
