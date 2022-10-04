4280b4ce349324eb3da0515d97ef880b28768360
JunsuMacbook <krpjs0508@gmail.com>
2022-10-04 16:35:36 +0900

    feat :: DialogUtil 6

--- app/src/main/java/com/teamnk/kimiljung/util/DialogUtil.kt
+++ app/src/main/java/com/teamnk/kimiljung/util/DialogUtil.kt
@@ -1,40 +1,27 @@
 package com.teamnk.kimiljung.util
 
-import android.app.Activity
-import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
-import android.view.ViewGroup.LayoutParams.MATCH_PARENT
-import android.view.WindowManager
-import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.databinding.DialogAllSingleButtonBinding
 
-// TODO title, descriptions parameter
-fun showDialogWithSingleButton(context: Context) {
+fun showDialogWithSingleButton(context: Context, title: String, description: String) {
     val binding: DialogAllSingleButtonBinding by lazy {
         DialogAllSingleButtonBinding.inflate(
             LayoutInflater.from(context)
         )
     }
 
-    /*val dialogBuilder = AlertDialog.Builder(context, R.layout.dialog_all_single_button)
-        .setView(binding.root)
-        .setCancelable(false)
-        .create()*/
-
     val dialog = Dialog(context)
-    dialog.setContentView(binding.root)
-    dialog.setCancelable(false)
+    with(dialog) {
+        setContentView(binding.root)
+        setCancelable(false)
+        show()
+    }
 
-    dialog.show()
-
-    // TODO redefine things with parameter
-    binding.tvDialogSingleTitle.setText(R.string.dialog_register_success_title)
-    binding.tvDialogSingleDescription.setText(R.string.dialog_register_success_description)
+    binding.tvDialogSingleTitle.text = title
+    binding.tvDialogSingleDescription.text = description
     binding.btnDialogAccept.setOnClickListener {
-        dialog.cancel()
+        // todo 메인액티비티 로직
     }
 }
