3320ee4e4bbf734eccb71a5a89018db6018ae05d
JunsuMacbook <krpjs0508@gmail.com>
2022-10-04 12:47:04 +0900

    feat :: DialogUtil 3

--- app/src/main/java/com/teamnk/kimiljung/util/DialogUtil.kt
+++ app/src/main/java/com/teamnk/kimiljung/util/DialogUtil.kt
@@ -4,23 +4,28 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
-import androidx.databinding.DataBindingUtil
 import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.databinding.DialogAllSingleButtonBinding
 
+// TODO title, descriptions parameter
 fun showDialogWithSingleButton(context: Context, activity: Activity) {
     val binding: DialogAllSingleButtonBinding by lazy {
-        DataBindingUtil.inflate(
-            LayoutInflater.from(context),
-            R.layout.dialog_all_single_button,
-            null,
-            false
+        DialogAllSingleButtonBinding.inflate(
+            LayoutInflater.from(context)
         )
     }
-    val dialogBuilder = AlertDialog.Builder(activity, R.style.SingleButtonAlertDialog)
+
+    val dialogBuilder = AlertDialog.Builder(context)
         .setView(binding.root)
+        .setCancelable(false)
         .create()
+
+    // TODO redefine things with parameter
     binding.tvDialogSingleTitle.setText(R.string.dialog_register_success_title)
     binding.tvDialogSingleDescription.setText(R.string.dialog_register_success_description)
+    binding.btnDialogAccept.setOnClickListener {
+        dialogBuilder.cancel()
+    }
+
     dialogBuilder.show()
 }
