8a3eb55e42e45b8d477faa1bb380137d35c72c94
Tmdhoon2 <hks026naver@gmail.com>
2022-10-07 08:25:51 +0900

    feat :: email verify button

--- app/src/main/java/com/teamnk/kimiljung/activity/auth/RegisterActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/activity/auth/RegisterActivity.kt
@@ -1,19 +1,25 @@
 package com.teamnk.kimiljung.activity.auth
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.base.BaseActivity
 import com.teamnk.kimiljung.databinding.ActivityRegisterBinding
 import com.teamnk.kimiljung.util.showDialogWithSingleButton
 import com.teamnk.kimiljung.util.startIntent
+import com.teamnk.kimiljung.viewmodel.RegisterViewModel
 
 class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
     R.layout.activity_register
 ) {
+
+    private val viewModel by viewModels<RegisterViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initNextButton()
+        initEmailVerifyButton()
     }
 
     private fun initNextButton() {
@@ -23,6 +29,20 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
                 getString(R.string.dialog_register_success_title),
                 getString(R.string.dialog_register_success_description)
             ) { startIntent(this, LoginActivity::class.java) }
+        }
+    }
+
+    private fun initEmailVerifyButton() {
+        binding.btnRegisterVerifyEmail.setOnClickListener {
+            val email = binding.etRegisterEmail.text.toString()
+            if (email.isNotBlank()) {
+                // TODO Verify Logic
+                viewModel.run {
+
+                }
+            } else {
+
+            }
         }
     }
 }
