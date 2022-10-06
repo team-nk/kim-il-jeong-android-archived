7e053233fcefa1a938e40ad2b361dd66449a1f8a
Tmdhoon2 <hks026naver@gmail.com>
2022-10-06 17:18:47 +0900

    feat :: ErrorPrint

--- app/src/main/java/com/teamnk/kimiljung/util/ToastUtil.kt
+++ app/src/main/java/com/teamnk/kimiljung/util/ToastUtil.kt
@@ -6,3 +6,7 @@ import android.widget.Toast
 fun print(context: Context, message : String){
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
+
+fun errorPrint(context: Context){
+    Toast.makeText(context, "서버 통신 에러", Toast.LENGTH_SHORT).show()
+}
