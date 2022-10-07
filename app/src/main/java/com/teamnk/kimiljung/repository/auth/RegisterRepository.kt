eb3ef03554193d54656cc1e01a114d8e6261226e
junsuPark <101160207+JunJaBoy@users.noreply.github.com>
2022-10-07 16:11:17 +0900

    Create RegisterRepository.kt

--- app/src/main/java/com/teamnk/kimiljung/repository/auth/RegisterRepository.kt
+++ app/src/main/java/com/teamnk/kimiljung/repository/auth/RegisterRepository.kt
@@ -1,10 +1,9 @@
 package com.teamnk.kimiljung.repository.auth
-
 import com.teamnk.kimiljung.data.RegisterApi
 import com.teamnk.kimiljung.dto.DuplicateRequest
 import com.teamnk.kimiljung.dto.VerifyCodeRequest
 import com.teamnk.kimiljung.dto.VerifyRequest
-
+// TODO refactor
 class RegisterRepository(
     private val registerApi: RegisterApi
 ) {
