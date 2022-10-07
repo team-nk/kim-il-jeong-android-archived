6f4629e67b009a9154b2e6b325e80f5a2c8e645c
JunsuMacbook <krpjs0508@gmail.com>
2022-10-07 15:25:50 +0900

    feat :: mainActivity dataBinding

--- app/src/main/java/com/teamnk/kimiljung/activity/MainActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/activity/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.teamnk.kimiljung.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.teamnk.kimiljung.R
+import com.teamnk.kimiljung.base.BaseActivity
+import com.teamnk.kimiljung.databinding.ActivityMainBinding
+import com.teamnk.kimiljung.fragment.CalendarFragment
 
-class MainActivity : AppCompatActivity() { // TODO Extend BaseActivity
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    R.layout.activity_main
+) {
+    val calendarFragment by lazy {
+        CalendarFragment()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initBottomNavigationView()
+    }
+
+    private fun initBottomNavigationView() {
+        binding.bnMain.run {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.bn_main_calendar -> {
+                        changeFragment(calendarFragment)
+                    }
+                }
+                true
+            }
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_main, fragment)
+            .commit()
     }
 }
