d5fe03836a0ca7da53c74c7c6c11fc328c4b6d43
JunsuMacbook <krpjs0508@gmail.com>
2022-10-07 23:44:41 +0900

    feat :: loginViewModel logic

--- app/src/main/java/com/teamnk/kimiljung/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/teamnk/kimiljung/viewmodel/LoginViewModel.kt
@@ -1,35 +1,27 @@
 package com.teamnk.kimiljung.viewmodel
 
-import android.content.ContentValues.TAG
-import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.teamnk.kimiljung.data.dto.LoginRequest
 import com.teamnk.kimiljung.repository.auth.LoginRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class LoginViewModel(
     private val rp: LoginRepository
 ) : ViewModel() {
-    private var success: MutableLiveData<Boolean> = MutableLiveData()
-    private val failed: MutableLiveData<Boolean> = MutableLiveData()
+    var success: MutableLiveData<Boolean> = MutableLiveData()
+    val failed: MutableLiveData<Boolean> = MutableLiveData()
 
-    private var id: MutableLiveData<String> = MutableLiveData()
-    private var password: MutableLiveData<String> = MutableLiveData()
-
-    suspend fun postLogin(loginRequest: LoginRequest) {
-        kotlin.runCatching {
-            withContext(Dispatchers.Default) {
+    fun postLogin(loginRequest: LoginRequest) {
+        viewModelScope.launch {
+            kotlin.runCatching {
                 rp.login(loginRequest)
+            }.onSuccess {
+                success
+            }.onFailure {
+                failed
             }
-        }.onSuccess {
-            success
-        }.onFailure {
-            failed
         }
     }
 }
