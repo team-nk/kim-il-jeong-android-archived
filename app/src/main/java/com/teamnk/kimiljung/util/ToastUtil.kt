cb05890089e8bb537ebd66a00943e8b968ae8acc
junsuPark <101160207+JunJaBoy@users.noreply.github.com>
2022-10-07 16:15:01 +0900

    Update ToastUtil.kt

--- app/src/main/java/com/teamnk/kimiljung/util/ToastUtil.kt
+++ app/src/main/java/com/teamnk/kimiljung/util/ToastUtil.kt
@@ -2,7 +2,7 @@ package com.teamnk.kimiljung.util
 
 import android.content.Context
 import android.widget.Toast
-
+// TODO use snackBar instead of Toast
 fun print(context: Context, message : String){
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
