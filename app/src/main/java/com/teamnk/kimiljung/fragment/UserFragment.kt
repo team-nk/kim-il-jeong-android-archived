3c9a7e28bfec2a710eb4c11f97f44ebaabd1611a
JunsuMacbook <krpjs0508@gmail.com>
2022-10-07 16:54:29 +0900

    feat :: Fragments

---
+++ app/src/main/java/com/teamnk/kimiljung/fragment/UserFragment.kt
@@ -0,0 +1,22 @@
+package com.teamnk.kimiljung.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.teamnk.kimiljung.R
+import com.teamnk.kimiljung.base.BaseFragment
+import com.teamnk.kimiljung.databinding.FragmentCalendarBinding
+
+class UserFragment : BaseFragment<FragmentCalendarBinding>(
+    R.layout.fragment_user
+) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
