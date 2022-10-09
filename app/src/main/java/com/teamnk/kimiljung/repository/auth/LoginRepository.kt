b6125408e2390398819f346ae0ae824b42e1cd81
JunsuMacbook <krpjs0508@gmail.com>
2022-10-09 20:09:04 +0900

    .

--- app/src/main/java/com/teamnk/kimiljung/repository/auth/LoginRepository.kt
+++ app/src/main/java/com/teamnk/kimiljung/repository/auth/LoginRepository.kt
@@ -2,9 +2,9 @@ package com.teamnk.kimiljung.repository.auth
 
 import com.teamnk.kimiljung.data.dto.LoginRequest
 import com.teamnk.kimiljung.data.dto.LoginResponse
-import com.teamnk.kimiljung.data.loginApi
+import com.teamnk.kimiljung.data.loginAPI
 
 class LoginRepository {
     suspend fun login(loginRequest: LoginRequest): LoginResponse =
-        loginApi.login(loginRequest)
+        loginAPI.login(loginRequest)
 }
