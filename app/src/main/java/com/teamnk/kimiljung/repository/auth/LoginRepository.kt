a1b37f848908b9c9c47c219e263096b1b29f15d9
JunsuMacbook <krpjs0508@gmail.com>
2022-10-09 19:40:48 +0900

    refactor :: remove LoginRepository constructor

--- app/src/main/java/com/teamnk/kimiljung/repository/auth/LoginRepository.kt
+++ app/src/main/java/com/teamnk/kimiljung/repository/auth/LoginRepository.kt
@@ -4,8 +4,7 @@ import com.teamnk.kimiljung.data.dto.LoginRequest
 import com.teamnk.kimiljung.data.dto.LoginResponse
 import com.teamnk.kimiljung.data.loginApi
 
-class LoginRepository(
-) {
+class LoginRepository {
     suspend fun login(loginRequest: LoginRequest): LoginResponse =
         loginApi.login(loginRequest)
 }
