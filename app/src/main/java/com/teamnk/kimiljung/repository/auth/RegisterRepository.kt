396622483641d5068551aa3022e2393278761e9c
JunsuMacbook <krpjs0508@gmail.com>
2022-10-09 20:09:06 +0900

    .

--- app/src/main/java/com/teamnk/kimiljung/repository/auth/RegisterRepository.kt
+++ app/src/main/java/com/teamnk/kimiljung/repository/auth/RegisterRepository.kt
@@ -1,11 +1,11 @@
 package com.teamnk.kimiljung.repository.auth
-import com.teamnk.kimiljung.data.RegisterApi
+import com.teamnk.kimiljung.data.RegisterAPI
 import com.teamnk.kimiljung.data.dto.UserIdDuplicationRequest
 import com.teamnk.kimiljung.data.dto.EmailVerificationCodeRequest
 import com.teamnk.kimiljung.data.dto.EmailVerificationRequest
 // TODO refactor
 class RegisterRepository(
-    private val registerApi: RegisterApi
+    private val registerApi: RegisterAPI
 ) {
     suspend fun verifyEmail(emailVerificationRequest: EmailVerificationRequest) =
         registerApi.verifyEmail(emailVerificationRequest)
