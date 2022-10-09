335b1a584f4bfd923ca6af2f9383de884b01248d
JunsuMacbook <krpjs0508@gmail.com>
2022-10-09 19:25:40 +0900

    feat :: APIProvider

---
+++ app/src/main/java/com/teamnk/kimiljung/data/APIProvider.kt
@@ -0,0 +1,11 @@
+package com.teamnk.kimiljung.data
+
+import retrofit2.Retrofit
+
+private val retrofit: Retrofit = Retrofit.Builder().apply {
+    baseUrl(BASE_URL)
+}.build()
+
+val loginApi: LoginApi by lazy {
+    retrofit.create(LoginApi::class.java)
+}
