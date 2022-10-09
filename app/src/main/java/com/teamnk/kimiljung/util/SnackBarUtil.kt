d8f0da38b02ac7732971701be7a0c1fb5924217a
JunsuMacbook <krpjs0508@gmail.com>
2022-10-09 20:16:01 +0900

    refactor :: SnackBarUtil

--- app/src/main/java/com/teamnk/kimiljung/util/SnackBarUtil.kt
+++ app/src/main/java/com/teamnk/kimiljung/util/SnackBarUtil.kt
@@ -2,13 +2,15 @@ package com.teamnk.kimiljung.util
 
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.LENGTH_LONG
+import com.google.android.material.snackbar.Snackbar.LENGTH_SHORT
 
 fun showShortSnackBar(view: View, text: String){
-    showSnackBar(view, text, Snackbar.LENGTH_SHORT)
+    showSnackBar(view, text, LENGTH_SHORT)
 }
 
 fun showLongSnackBar(view: View, text: String){
-    showSnackBar(view, text, Snackbar.LENGTH_LONG)
+    showSnackBar(view, text, LENGTH_LONG)
 }
 
 fun showSnackBar(view: View, text: String, length: Int) {
