efbee2551d99cd4659fb33e50501293d732dce23
JunsuMacbook <krpjs0508@gmail.com>
2022-10-09 19:41:18 +0900

    feat :: disable LoginViewModelFactory

--- app/src/main/java/com/teamnk/kimiljung/viewmodel/LoginViewModelFactory.kt
+++ app/src/main/java/com/teamnk/kimiljung/viewmodel/LoginViewModelFactory.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.teamnk.kimiljung.repository.auth.LoginRepository
 
+/*
 class LoginViewModelFactory(private val loginRepository: LoginRepository) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return LoginViewModel(loginRepository) as T
     }
-}
+}*/
