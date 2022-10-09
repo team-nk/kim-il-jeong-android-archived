22cb1c1ffc4e62f3cc4ca737665e3510ff8da703
JunsuMacbook <krpjs0508@gmail.com>
2022-10-09 19:26:47 +0900

    feat :: remove LoginActivity LoginApi

--- app/src/main/java/com/teamnk/kimiljung/repository/auth/LoginRepository.kt
+++ app/src/main/java/com/teamnk/kimiljung/repository/auth/LoginRepository.kt
@@ -1,11 +1,10 @@
 package com.teamnk.kimiljung.repository.auth
 
-import com.teamnk.kimiljung.data.LoginApi
 import com.teamnk.kimiljung.data.dto.LoginRequest
 import com.teamnk.kimiljung.data.dto.LoginResponse
+import com.teamnk.kimiljung.data.loginApi
 
 class LoginRepository(
-    private val loginApi: LoginApi
 ) {
     suspend fun login(loginRequest: LoginRequest): LoginResponse =
         loginApi.login(loginRequest)
