73ae6144c9a1957f9090995965ed8c6d0f6bf06f
JunsuMacbook <krpjs0508@gmail.com>
2022-10-12 07:33:20 +0900

    refactor :: fragment 이름 변경

--- app/src/main/java/com/teamnk/kimiljung/ui/fragment/UserFragment.kt
+++ app/src/main/java/com/teamnk/kimiljung/ui/fragment/UserFragment.kt
@@ -1,15 +1,13 @@
 package com.teamnk.kimiljung.ui.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.base.BaseFragment
 import com.teamnk.kimiljung.databinding.FragmentCalendarBinding
 
 class UserFragment : BaseFragment<FragmentCalendarBinding>(
-    R.layout.fragment_user
+    R.layout.fragment_mypage
 ) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
