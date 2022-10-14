381e3fb0b6d2b7ec9280797e36852e0f66c4c6bc
JunsuMacbook <krpjs0508@gmail.com>
2022-10-14 14:27:15 +0900

    .

--- app/src/main/java/com/teamnk/kimiljung/util/Constans.kt
+++ app/src/main/java/com/teamnk/kimiljung/util/Constans.kt
@@ -3,12 +3,12 @@ package com.teamnk.kimiljung.util
 object SharedPreferencesName {
     const val INTRODUCTION_PAGER_ACTIVITY = "introductionPagerActivity"
 
-    const val LOGIN_ACTIVITY = "loginActivity"
+    const val MAIN_ACTIVITY = "mainActivity"
 }
 
 object SharedPreferencesKey {
     const val INTRODUCTION_PAGER_IS_INTRODUCTION_PAGER_ACTIVITY_SHOWN = "isIntroductionPagerActivityShown"
 
-    const val LOGIN_ACTIVITY_SAVED_FRAGMENT_ID = "savedFragmentId"
+    const val MAIN_ACTIVITY_SAVED_FRAGMENT_ID = "savedFragmentId"
 }
 
