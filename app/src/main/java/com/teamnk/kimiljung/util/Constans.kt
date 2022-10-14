960ac11632d4c32bec5d060c1393a486da7986dc
JunsuMacbook <krpjs0508@gmail.com>
2022-10-14 15:02:26 +0900

    feat :: MainActivity SharedPreferences Keys

--- app/src/main/java/com/teamnk/kimiljung/util/Constans.kt
+++ app/src/main/java/com/teamnk/kimiljung/util/Constans.kt
@@ -1,5 +1,7 @@
 package com.teamnk.kimiljung.util
 
+import com.teamnk.kimiljung.R
+
 object SharedPreferencesName {
     const val INTRODUCTION_PAGER_ACTIVITY = "introductionPagerActivity"
 
@@ -7,8 +9,13 @@ object SharedPreferencesName {
 }
 
 object SharedPreferencesKey {
-    const val INTRODUCTION_PAGER_IS_INTRODUCTION_PAGER_ACTIVITY_SHOWN = "isIntroductionPagerActivityShown"
+    const val INTRODUCTION_PAGER_IS_INTRODUCTION_PAGER_ACTIVITY_SHOWN =
+        "isIntroductionPagerActivityShown"
 
-    const val MAIN_ACTIVITY_SAVED_FRAGMENT_ID = "savedFragmentId"
+    const val MAIN_ACTIVITY_SAVED_BOTTOM_NAVIGATION_ID = "savedBottomNavigationId"
+    const val BOTTOM_NAVIGATION_CALENDAR_ID = R.id.bn_main_calendar
+    const val BOTTOM_NAVIGATION_MAP_ID = R.id.bn_main_map
+    const val BOTTOM_NAVIGATION_NOTIFICATION_ID = R.id.bn_main_notification
+    const val BOTTOM_NAVIGATION_MY_PAGE_ID = R.id.bn_main_mypage
 }
 
