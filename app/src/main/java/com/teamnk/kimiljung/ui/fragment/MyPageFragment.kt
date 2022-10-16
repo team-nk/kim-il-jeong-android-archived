edf6c9330b1318fd792a372538c436efa374e2a3
JunsuMacbook <krpjs0508@gmail.com>
2022-10-16 22:52:30 +0900

    feat :: initial functions

--- app/src/main/java/com/teamnk/kimiljung/ui/fragment/MyPageFragment.kt
+++ app/src/main/java/com/teamnk/kimiljung/ui/fragment/MyPageFragment.kt
@@ -13,6 +13,19 @@ class MyPageFragment : BaseFragment<FragmentCalendarBinding>(
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initPersonalInformationButtons()
+        initApplicationInformationButtons()
+    }
+
+    // 유저 프로필 정보, 내 일정 확인하기, 내가 쓴 게시물 보기 등 개인 정보를 담고 있는 레이아웃과 내부 버튼들을 초기화하는 함수
+    private fun initPersonalInformationButtons() {
+
+    }
+
+    // 생년월일 입력하기/수정하기, 로그아웃 등의 버튼을 초기화하는 함수
+    private fun initApplicationInformationButtons() {
+        
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
