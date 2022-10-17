b17065659c5346387784a9c9d3bd28c246a1d706
JunsuMacbook <krpjs0508@gmail.com>
2022-10-17 18:35:19 +0900

    fix :: MyPageFragment error handling

--- app/src/main/java/com/teamnk/kimiljung/ui/fragment/MyPageFragment.kt
+++ app/src/main/java/com/teamnk/kimiljung/ui/fragment/MyPageFragment.kt
@@ -2,9 +2,7 @@ package com.teamnk.kimiljung.ui.fragment
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.base.BaseFragment
 import com.teamnk.kimiljung.databinding.FragmentMypageBinding
@@ -25,16 +23,11 @@ class MyPageFragment : BaseFragment<FragmentMypageBinding>(
         mainActivity = context as MainActivity
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         initPersonalInformationButtons()
         initInteractButtons()
-
-        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
     // 유저 프로필 정보, 내 일정 확인하기, 내가 쓴 게시물 보기 등 개인 정보를 담고 있는 레이아웃과 내부 버튼들을 초기화하는 함수
