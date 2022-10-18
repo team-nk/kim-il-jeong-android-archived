c8362e0e078b3daf6cde8528d1dc1f188b501ca6
JunsuMacbook <krpjs0508@gmail.com>
2022-10-18 18:45:29 +0900

    .

--- app/src/main/java/com/teamnk/kimiljung/ui/activity/auth/StartActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/ui/activity/auth/StartActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.base.BaseActivity
 import com.teamnk.kimiljung.databinding.ActivityStartBinding
-import com.teamnk.kimiljung.util.SharedPreferencesKey
+import com.teamnk.kimiljung.util.SharedPreferencesKey.IS_INTRODUCTION_PAGER_ACTIVITY_SHOWN
+import com.teamnk.kimiljung.util.SharedPreferencesName.USER_AUTH
+import com.teamnk.kimiljung.util.getSharedPreferencesEditor
+import com.teamnk.kimiljung.util.initializeSharedPreferences
 import com.teamnk.kimiljung.util.putInSharedPreferences
 import com.teamnk.kimiljung.util.startIntent
 
 class StartActivity : BaseActivity<ActivityStartBinding>(
     R.layout.activity_start
 ) {
+    private val sharedPreferences by lazy {
+        initializeSharedPreferences(this, USER_AUTH, MODE_PRIVATE)
+    }
+
+    private val sharedPreferencesEditor by lazy {
+        getSharedPreferencesEditor(sharedPreferences)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,6 +31,12 @@ class StartActivity : BaseActivity<ActivityStartBinding>(
     private fun initStartLoginText() {
         binding.tvStartLoginWithEmail.setOnClickListener {
             startIntent(this, LoginActivity::class.java)
+
+            putInSharedPreferences(
+                sharedPreferencesEditor,
+                IS_INTRODUCTION_PAGER_ACTIVITY_SHOWN,
+                true
+            )
         }
     }
 
