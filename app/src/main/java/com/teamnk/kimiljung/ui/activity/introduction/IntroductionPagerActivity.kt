8f9eec17d20fa027b29009b0ef7b03f518a85538
JunsuMacbook <krpjs0508@gmail.com>
2022-10-18 18:45:23 +0900

    feat :: convert sharedPreferences in StartActivity

--- app/src/main/java/com/teamnk/kimiljung/ui/activity/introduction/IntroductionPagerActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/ui/activity/introduction/IntroductionPagerActivity.kt
@@ -6,11 +6,11 @@ import com.teamnk.kimiljung.base.BaseActivity
 import com.teamnk.kimiljung.databinding.ActivityIntroductionPagerBinding
 import com.teamnk.kimiljung.ui.activity.auth.StartActivity
 import com.teamnk.kimiljung.ui.adapter.introduction.IntroductionPagerAdapter
-import com.teamnk.kimiljung.util.SharedPreferencesKey.INTRODUCTION_PAGER_IS_INTRODUCTION_PAGER_ACTIVITY_SHOWN
+import com.teamnk.kimiljung.util.SharedPreferencesKey.IS_INTRODUCTION_PAGER_ACTIVITY_SHOWN
 import com.teamnk.kimiljung.util.SharedPreferencesName.INTRODUCTION_PAGER_ACTIVITY
+import com.teamnk.kimiljung.util.SharedPreferencesName.USER_AUTH
 import com.teamnk.kimiljung.util.getSharedPreferencesEditor
 import com.teamnk.kimiljung.util.initializeSharedPreferences
-import com.teamnk.kimiljung.util.putInSharedPreferences
 import com.teamnk.kimiljung.util.startIntentClearTop
 
 class IntroductionPagerActivity : BaseActivity<ActivityIntroductionPagerBinding>(
@@ -18,7 +18,7 @@ class IntroductionPagerActivity : BaseActivity<ActivityIntroductionPagerBinding>
 ) {
 
     private val sharedPreferences by lazy {
-        initializeSharedPreferences(this, INTRODUCTION_PAGER_ACTIVITY, MODE_PRIVATE)
+        initializeSharedPreferences(this, USER_AUTH, MODE_PRIVATE)
     }
     private val sharedPreferencesEditor by lazy {
         getSharedPreferencesEditor(sharedPreferences)
@@ -35,7 +35,7 @@ class IntroductionPagerActivity : BaseActivity<ActivityIntroductionPagerBinding>
 
     private fun checkIntroductionPageShown() {
         if (sharedPreferences.getBoolean(
-                INTRODUCTION_PAGER_IS_INTRODUCTION_PAGER_ACTIVITY_SHOWN,
+                IS_INTRODUCTION_PAGER_ACTIVITY_SHOWN,
                 false
             )
         ) {
