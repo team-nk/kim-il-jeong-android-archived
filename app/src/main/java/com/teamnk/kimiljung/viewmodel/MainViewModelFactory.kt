4210155993a939ed26fd6c70ad14a34803b3cc89
JunsuMacbook <krpjs0508@gmail.com>
2022-10-21 20:21:00 +0900

    refactor :: refactor LoginViewModelFactory

--- app/src/main/java/com/teamnk/kimiljung/viewmodel/MainViewModelFactory.kt
+++ app/src/main/java/com/teamnk/kimiljung/viewmodel/MainViewModelFactory.kt
@@ -4,7 +4,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.teamnk.kimiljung.data.repository.MainRepository
 
-class MainViewModelFactory(private val mainRepository: MainRepository) : ViewModelProvider.Factory {
+class MainViewModelFactory(private val mainRepository: MainRepository) :
+    ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return MainViewModel(mainRepository) as T
     }
