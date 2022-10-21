678a6fda1d055e4a20fc96c8cca560f119b7093b
JunsuMacbook <krpjs0508@gmail.com>
2022-10-21 23:15:49 +0900

    refactor :: calendar class

--- app/src/main/java/com/teamnk/kimiljung/ui/fragment/CalendarFragment.kt
+++ app/src/main/java/com/teamnk/kimiljung/ui/fragment/CalendarFragment.kt
@@ -1,48 +1,51 @@
 package com.teamnk.kimiljung.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import com.prolificinteractive.materialcalendarview.CalendarDay
-import com.prolificinteractive.materialcalendarview.MaterialCalendarView
-import com.prolificinteractive.materialcalendarview.OnDateSelectedListener
 import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.base.BaseFragment
 import com.teamnk.kimiljung.databinding.FragmentCalendarBinding
 import com.teamnk.kimiljung.ui.DecorateCalendarView
+import com.teamnk.kimiljung.ui.activity.MainActivity
 
 class CalendarFragment : BaseFragment<FragmentCalendarBinding>(
     R.layout.fragment_calendar
 ) {
 
+    private lateinit var mainActivity: MainActivity
+
     private var today = CalendarDay.today()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        mainActivity = context as MainActivity
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.calendarCalendarView.addDecorator(DecorateCalendarView(this.requireContext()))
+        mainActivity = context as MainActivity
 
-        binding.calendarCalendarView.setOnDateChangedListener(object : OnDateSelectedListener{
-            override fun onDateSelected(
-                widget: MaterialCalendarView,
-                date: CalendarDay,
-                selected: Boolean
-            ) {
-                Log.d("TEST", date.toString())
-                if(date == today){
-                    binding.calendarCalendarView.removeDecorator(DecorateCalendarView(this@CalendarFragment.requireContext()))
-                    Log.d("TEST", "today")
-                }else {
-                    binding.calendarCalendarView.addDecorator(DecorateCalendarView(this@CalendarFragment.requireContext()))
-                }
-            }
-
-        })
-
+        initCalendarView()
     }
 
+    private fun initCalendarView() {
+        with(binding.calendarCalendarView) {
+            addDecorator(DecorateCalendarView(mainActivity))
+            setOnDateChangedListener { _, date, _ ->
+                Log.d("TEST", date.toString())
+                if (date == today) {
+                    removeDecorator(DecorateCalendarView(mainActivity))
+                    Log.d("TEST", "today")
+                } else {
+                    addDecorator(DecorateCalendarView(mainActivity))
+                }
+            }
+        }
+    }
 
 
     override fun observeEvent() {
