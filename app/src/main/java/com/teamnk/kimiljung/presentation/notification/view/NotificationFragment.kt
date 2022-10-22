d7475d4f4911a59465d65a86970554a2415909e9
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 10:00:53 +0900

    temp

---
+++ app/src/main/java/com/teamnk/kimiljung/presentation/notification/view/NotificationFragment.kt
@@ -0,0 +1,13 @@
+package com.teamnk.kimiljung.presentation.notification.view
+
+import com.teamnk.kimiljung.R
+import com.teamnk.kimiljung.presentation.base.BaseFragment
+import com.teamnk.kimiljung.databinding.FragmentCalendarBinding
+
+class NotificationFragment : BaseFragment<FragmentCalendarBinding>(
+    R.layout.fragment_notification
+) {
+
+    override fun observeEvent() {
+    }
+}
