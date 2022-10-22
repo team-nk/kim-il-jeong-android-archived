966c28822e11e471e5286c9b517a33d9d7d9e82c
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 13:28:09 +0900

    feat :: LoginViewModel

--- app/src/main/java/com/teamnk/kimiljung/presentation/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/teamnk/kimiljung/presentation/login/viewmodel/LoginViewModel.kt
@@ -2,6 +2,7 @@ package com.teamnk.kimiljung.presentation.login.viewmodel
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -13,19 +14,24 @@ class LoginViewModel(
     private val repository: LoginRepository
 ) : ViewModel() {
 
-    var success: MutableLiveData<Boolean> = MutableLiveData()
-    var failure: MutableLiveData<Boolean> = MutableLiveData()
+    var _success = MutableLiveData<Boolean>()
+    var _failure = MutableLiveData<Boolean>()
+
+    val success: LiveData<Boolean>
+        get() = _success
+    val failure: LiveData<Boolean>
+        get() = _failure
 
     fun postLogin(loginRequest: LoginRequest) {
         viewModelScope.launch {
             kotlin.runCatching {
                 repository.login(loginRequest)
             }.onSuccess {
-                success
+                _success
             }.onFailure {
-                failure
+                _failure
             }.also {
-                Log.d(TAG, "$success, $failure")
+                Log.d(TAG, "$_success, $_failure")
             }
         }
     }
