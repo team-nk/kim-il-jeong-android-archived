67d97c500b2966ea109a85ea687d91440b633294
Junsu <krpjs0508@gmail.com>
2022-10-31 22:46:35 +0900

    feat :: BaseFragment

--- app/src/main/java/com/teamnk/kimiljung/presentation/base/BaseFragment.kt
+++ app/src/main/java/com/teamnk/kimiljung/presentation/base/BaseFragment.kt
@@ -5,13 +5,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import com.teamnk.kimiljung.util.SharedPreferencesName
+import com.teamnk.kimiljung.util.initializeSharedPreferences
 
 abstract class BaseFragment<B : ViewDataBinding>(
     @LayoutRes private val layoutId: Int
 ) : Fragment() {
+
+    protected val defaultSharedPreferences by lazy {
+        initializeSharedPreferences(
+            this.requireActivity(),
+            SharedPreferencesName.DEFAULT,
+            AppCompatActivity.MODE_PRIVATE
+        )
+    }
+
+    protected val defaultSharedPreferencesEditor by lazy {
+        defaultSharedPreferences.edit()
+    }
+
+    protected val userAuthSharedPreferences by lazy {
+        initializeSharedPreferences(
+            this.requireActivity(),
+            SharedPreferencesName.USER_AUTH,
+            AppCompatActivity.MODE_PRIVATE
+        )
+    }
+
+    protected val userAuthSharedPreferencesEditor by lazy {
+        userAuthSharedPreferences.edit()
+    }
 
     protected lateinit var binding: B
 
