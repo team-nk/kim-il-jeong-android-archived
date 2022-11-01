03587010beafe5b257a9a60bbde37702b7d0733e
Junsu <krpjs0508@gmail.com>
2022-11-01 14:22:59 +0900

    refactor :: LoginRepository return type

--- app/src/main/java/com/teamnk/kimiljung/data/repository/login/LoginRepository.kt
+++ app/src/main/java/com/teamnk/kimiljung/data/repository/login/LoginRepository.kt
@@ -1,10 +1,11 @@
 package com.teamnk.kimiljung.data.repository.login
 
+import com.teamnk.kimiljung.data.api.loginAPI
 import com.teamnk.kimiljung.data.model.login.LoginRequest
 import com.teamnk.kimiljung.data.model.login.LoginResponse
-import com.teamnk.kimiljung.data.loginAPI
+import retrofit2.Response
 
 class LoginRepository {
-    suspend fun login(loginRequest: LoginRequest): LoginResponse =
+    suspend fun login(loginRequest: LoginRequest): Response<LoginResponse> =
         loginAPI.login(loginRequest)
 }
