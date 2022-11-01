ec8980af2427b96bf9566249b454b42d96767a22
Junsu <krpjs0508@gmail.com>
2022-11-01 14:23:12 +0900

    refactor :: LoginViewModel Name

--- app/src/main/java/com/teamnk/kimiljung/presentation/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/teamnk/kimiljung/presentation/login/viewmodel/LoginViewModel.kt
@@ -1,12 +1,11 @@
 package com.teamnk.kimiljung.presentation.login.viewmodel
 
-import android.content.ContentValues.TAG
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.teamnk.kimiljung.data.model.login.LoginRequest
+import com.teamnk.kimiljung.data.model.login.LoginResponse
 import com.teamnk.kimiljung.data.repository.login.LoginRepository
 import kotlinx.coroutines.launch
 
@@ -14,24 +13,21 @@ class LoginViewModel(
     private val repository: LoginRepository
 ) : ViewModel() {
 
-    var _success = MutableLiveData<Boolean>()
-    var _failure = MutableLiveData<Boolean>()
-
-    val success: LiveData<Boolean>
-        get() = _success
-    val failure: LiveData<Boolean>
-        get() = _failure
+    private var _loginResponse = MutableLiveData<LoginResponse>()
+    val loginResponse: LiveData<LoginResponse> = _loginResponse
 
     fun postLogin(loginRequest: LoginRequest) {
         viewModelScope.launch {
             kotlin.runCatching {
                 repository.login(loginRequest)
             }.onSuccess {
-                _success
+                if (it.isSuccessful) {
+                    _loginResponse.postValue(it.body())
+                } else {
+                    println("login error = ${it.errorBody()}")
+                }
             }.onFailure {
-                _failure
-            }.also {
-                Log.d(TAG, "$_success, $_failure")
+                println("login failure")
             }
         }
     }
