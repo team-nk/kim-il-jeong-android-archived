b29e3fe72bd1a4d86efb3b313377ff902e3aa941
Junsu <krpjs0508@gmail.com>
2022-11-01 11:45:48 +0900

    feat :: LoginRequest SerializedName

--- app/src/main/java/com/teamnk/kimiljung/data/model/login/LoginRequest.kt
+++ app/src/main/java/com/teamnk/kimiljung/data/model/login/LoginRequest.kt
@@ -1,6 +1,10 @@
 package com.teamnk.kimiljung.data.model.login
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginRequest(
+    @SerializedName("email")
     val email: String,
+    @SerializedName("password")
     val password: String,
 )
