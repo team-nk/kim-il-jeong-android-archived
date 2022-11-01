0d767473ee743fd36ffb721e4390f9af12c8b7c2
Junsu <krpjs0508@gmail.com>
2022-11-01 14:21:24 +0900

    refactor :: remove not-used function

--- app/src/main/java/com/teamnk/kimiljung/presentation/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/teamnk/kimiljung/presentation/main/viewmodel/MainViewModel.kt
@@ -4,9 +4,4 @@ import androidx.lifecycle.ViewModel
 import com.teamnk.kimiljung.data.repository.main.MainRepository
 
 class MainViewModel(private val repository: MainRepository) : ViewModel() {
-
-    // TODO 로그아웃 로직 구현
-    fun logOut() {
-
-    }
 }
