8ba225ec734954994454c031d2fb950d4e45a1a0
Junsu <krpjs0508@gmail.com>
2022-11-02 10:30:15 +0900

    refactor :: LoginRequest

--- app/src/main/java/com/teamnk/kimiljung/data/model/login/LoginRequest.kt
+++ app/src/main/java/com/teamnk/kimiljung/data/model/login/LoginRequest.kt
@@ -3,8 +3,6 @@ package com.teamnk.kimiljung.data.model.login
 import com.google.gson.annotations.SerializedName
 
 data class LoginRequest(
-    @SerializedName("email")
-    val email: String,
-    @SerializedName("password")
-    val password: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("password") val password: String,
 )
