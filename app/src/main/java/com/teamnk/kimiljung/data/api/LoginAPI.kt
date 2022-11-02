2b5f3803ee3035f44e9a9d155dafe232cf0e62d1
Junsu <krpjs0508@gmail.com>
2022-11-02 10:30:04 +0900

    refactor :: api content-type

--- app/src/main/java/com/teamnk/kimiljung/data/api/LoginAPI.kt
+++ app/src/main/java/com/teamnk/kimiljung/data/api/LoginAPI.kt
@@ -4,11 +4,13 @@ import com.teamnk.kimiljung.data.model.login.LoginRequest
 import com.teamnk.kimiljung.data.model.login.LoginResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface LoginAPI {
 
-    @POST("login")
+    @POST("/user/login")
+    @Headers("Content-Type: application/json")
     suspend fun login(
         @Body loginRequest: LoginRequest
     ): Response<LoginResponse>
