5c6fb8b02f7f6ce17c38b463d8f9c61bf8dfad00
Junsu <krpjs0508@gmail.com>
2022-11-11 18:48:55 +0900

    fix :: conflict

--- app/src/main/java/com/teamnk/kimiljung/presentation/login/viewmodel/LoginViewModelFactory.kt
+++ app/src/main/java/com/teamnk/kimiljung/presentation/login/viewmodel/LoginViewModelFactory.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.teamnk.kimiljung.data.repository.login.LoginRepository
 
+@Suppress("UNCHECKED_CAST")
 class LoginViewModelFactory(private val loginRepository: LoginRepository) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
