c3820e81822816b662bf9b52bda6904523496af7
Junsu <krpjs0508@gmail.com>
2022-11-12 16:45:36 +0900

    refactor :: RegisterActivity

--- app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterActivity.kt
@@ -3,9 +3,8 @@ package com.teamnk.kimiljung.feature.register
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.teamnk.kimiljung.R
-import com.teamnk.kimiljung.databinding.ActivityRegisterBinding
 import com.teamnk.kimiljung.base.BaseActivity
-import com.teamnk.kimiljung.util.showDialogWithSingleButton
+import com.teamnk.kimiljung.databinding.ActivityRegisterBinding
 
 class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
     R.layout.activity_register
@@ -20,58 +19,25 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        initNextButton()
-        initEmailVerifyButton()
-        initCheckEmailVerifyButton()
-        initCheckUserIdDuplicate()
+        initWidgets()
     }
 
-    private fun initEmailVerifyButton() {
-        binding.btnRegisterVerifyEmail.setOnClickListener {
-            val email = binding.etRegisterEmail.text.toString()
-            if (email.isNotBlank()) {
-
-            } else {
-
-            }
-        }
+    private fun initWidgets() {
+        initVerifyEmailButton()
+        initCheckVerificationCodeButton()
+        initCheckIdDuplicationButton()
     }
 
-    private fun initCheckEmailVerifyButton() {
-        binding.btnRegisterCheckEmailVerified.setOnClickListener {
-            val key = binding.etRegisterVerificationCode.text
-            if (key.isNotBlank()) {
-
-            } else {
-
-            }
-        }
+    private fun initCheckIdDuplicationButton() {
+        // TODO 아이디 중복 확인 버튼
     }
 
-    private fun initCheckUserIdDuplicate() {
-        binding.btnRegisterCheckUserIdDuplicate.setOnClickListener {
-            val userId = binding.etRegisterUserId.text.toString()
-            if (userId.isNotBlank()) {
-                // TODO UserIdDuplicate Logic
-                viewModel.run {
-
-                }
-            } else {
-
-            }
-        }
+    private fun initCheckVerificationCodeButton() {
+        // TODO 인증번호 확인하기 버튼
     }
 
-    private fun initNextButton() {
-        binding.btnRegisterNext.setOnClickListener {
-            showDialogWithSingleButton(
-                this,
-                getString(R.string.dialog_register_success_title),
-                getString(R.string.dialog_register_success_description)
-            ) {
-                finish()
-            }
-        }
+    private fun initVerifyEmailButton() {
+        // TODO 이메일 인증
     }
 
     override fun observeEvent() {}
