75fc4d2e8d0f5aa3773e7624887250dada3cb5e0
Junsu <krpjs0508@gmail.com>
2022-11-12 18:04:33 +0900

    refactor :: registerData properties name

--- app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterData.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterData.kt
@@ -3,9 +3,9 @@ package com.teamnk.kimiljung.feature.register
 import com.google.gson.annotations.SerializedName
 
 data class RegisterRequest(
-    @SerializedName("accountId") val accountId: String,
+    @SerializedName("account_id") val accountId: String,
     @SerializedName("code") val code: Int,
     @SerializedName("email") val email: String,
     @SerializedName("password") val password: String,
-    @SerializedName("rePassword") val rePassword: String
+    @SerializedName("re_password") val rePassword: String
 )
