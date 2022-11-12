f127125b1762e4376de3283c41fcf1db36214932
Junsu <krpjs0508@gmail.com>
2022-11-12 16:32:58 +0900

    feat :: RegisterData and RegisterRequest

--- app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterData.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterData.kt
@@ -1,2 +1,11 @@
 package com.teamnk.kimiljung.feature.register
 
+import com.google.gson.annotations.SerializedName
+
+data class RegisterRequest(
+    @SerializedName("accountId") val accountId: String,
+    @SerializedName("code") val code: Int,
+    @SerializedName("email") val email: String,
+    @SerializedName("password") val password: String,
+    @SerializedName("rePassword") val rePassword: String
+)
