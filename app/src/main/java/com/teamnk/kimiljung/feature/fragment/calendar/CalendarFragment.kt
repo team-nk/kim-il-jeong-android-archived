1244f3d1a43cbc9448bffca0dc2a4cf40444ee19
Junsu <krpjs0508@gmail.com>
2022-11-14 18:27:55 +0900

    feat :: CalendarView month showing logic

--- app/src/main/java/com/teamnk/kimiljung/feature/fragment/calendar/CalendarFragment.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/fragment/calendar/CalendarFragment.kt
@@ -1,33 +1,22 @@
 package com.teamnk.kimiljung.feature.fragment.calendar
 
-import android.content.Context
 import android.os.Bundle
 import android.view.View
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter
-import com.prolificinteractive.materialcalendarview.format.TitleFormatter
 import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.base.BaseFragment
 import com.teamnk.kimiljung.databinding.FragmentCalendarBinding
-import com.teamnk.kimiljung.feature.main.MainActivity
 
 class CalendarFragment : BaseFragment<FragmentCalendarBinding>(
     R.layout.fragment_calendar
 ) {
 
-    private lateinit var mainActivity: MainActivity
-
     private var today = CalendarDay.today()
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-
-        mainActivity = context as MainActivity
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mainActivity = context as MainActivity
 
         initCalendarView()
         initCreateScheduleImage()
@@ -42,27 +31,18 @@ class CalendarFragment : BaseFragment<FragmentCalendarBinding>(
                 )
             )
         )
-        binding.calendarCalendarView.addDecorator(CalendarViewManager(mainActivity))
+        binding.calendarCalendarView.addDecorator(TodayDecorator(requireActivity()))
 
-        binding.calendarCalendarView.setTitleFormatter(object : TitleFormatter {
-            override fun format(day: CalendarDay): CharSequence {
-                val date = day.date
-                var month = day.month
-                var calendarHeaderElements = date.toString().split(" ")
-                month = month + 1
-                if (month == 13) month = 1
-                val calendarHeaderBuilder =
-                    StringBuilder().append("${calendarHeaderElements[5]} ${month}월")
-                return calendarHeaderBuilder.toString()
-            }
-        })
+        binding.calendarCalendarView.setTitleFormatter {
+            return@setTitleFormatter "${it.year}년 ${it.month + 1}월"
+        }
 
         with(binding.calendarCalendarView) {
             setOnDateChangedListener { _, date, _ ->
                 if (date == today) {
-                    removeDecorator(CalendarViewManager(mainActivity))
+                    removeDecorator(TodayDecorator(requireActivity()))
                 } else {
-                    addDecorator(CalendarViewManager(mainActivity))
+                    addDecorator(TodayDecorator(requireActivity()))
                 }
             }
         }
