5a87fdf72a54593a4113ce51b29d858223de3137
Junsu <krpjs0508@gmail.com>
2022-11-20 11:13:49 +0900

    feat :: ChangeUserInformationActivity file

---
+++ app/src/main/java/com/teamnk/kimiljung/ChangeUserInformationActivity.kt
@@ -0,0 +1,11 @@
+package com.teamnk.kimiljung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangeUserInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_user_information)
+    }
+}
