e78c69b1bd3617386664f6a39d1c1108c9cdc148
Junsu <krpjs0508@gmail.com>
2022-11-21 22:48:44 +0900

    feat :: create ChangePassword and activity_change_password

---
+++ app/src/main/java/com/teamnk/kimiljung/feature/changepassword/ChangePasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.teamnk.kimiljung.feature.changepassword
+
+import com.teamnk.kimiljung.R
+import com.teamnk.kimiljung.base.BaseActivity
+import com.teamnk.kimiljung.databinding.ActivityChangePasswordBinding
+
+class ChangePasswordActivity : BaseActivity<ActivityChangePasswordBinding>(
+    R.layout.activity_change_password
+) {
+    override fun observeEvent() {
+    }
+}
