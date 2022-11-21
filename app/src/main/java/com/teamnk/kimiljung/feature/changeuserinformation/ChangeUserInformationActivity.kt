ab6372c06df27ef41e3964bb41b9aa17e638d69a
Junsu <krpjs0508@gmail.com>
2022-11-21 19:30:44 +0900

    feat :: ChangeUserInformation revoke logic in FragmentMyPage

---
+++ app/src/main/java/com/teamnk/kimiljung/feature/changeuserinformation/ChangeUserInformationActivity.kt
@@ -0,0 +1,13 @@
+package com.teamnk.kimiljung.feature.changeuserinformation
+
+import com.teamnk.kimiljung.R
+import com.teamnk.kimiljung.base.BaseActivity
+import com.teamnk.kimiljung.databinding.ActivityChangeUserInformationBinding
+
+class ChangeUserInformationActivity : BaseActivity<ActivityChangeUserInformationBinding>(
+    R.layout.activity_change_user_information
+) {
+
+    override fun observeEvent() {
+    }
+}
--- app/src/main/java/com/teamnk/kimiljung/feature/fragment/mypage/MyPageFragment.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/fragment/mypage/MyPageFragment.kt
@@ -1,11 +1,16 @@
 package com.teamnk.kimiljung.feature.fragment.mypage
 
+import android.app.Activity
 import android.content.Context.MODE_PRIVATE
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.teamnk.kimiljung.R
-import com.teamnk.kimiljung.databinding.FragmentMypageBinding
 import com.teamnk.kimiljung.base.BaseFragment
+import com.teamnk.kimiljung.databinding.FragmentMypageBinding
+import com.teamnk.kimiljung.feature.changeuserinformation.ChangeUserInformationActivity
 import com.teamnk.kimiljung.feature.start.StartActivity
 import com.teamnk.kimiljung.util.SharedPreferencesName.DEFAULT
 import com.teamnk.kimiljung.util.SharedPreferencesName.USER_AUTH
@@ -17,14 +22,35 @@ class MyPageFragment : BaseFragment<FragmentMypageBinding>(
     R.layout.fragment_mypage
 ) {
 
+    private lateinit var changeUserInformationActivityResultLauncher: ActivityResultLauncher<Intent>
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        changeUserInformationActivityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == Activity.RESULT_OK) {
+                    //TODO 유저 정보 재호출 로직
+                }
+            }
 
         initPersonalInformationButtons()
         initInteractButtons()
     }
 
     private fun initPersonalInformationButtons() {
+        initEditProfileButton()
+    }
+
+    private fun initEditProfileButton() {
+        binding.imgMypageEditProfile.setOnClickListener {
+            changeUserInformationActivityResultLauncher.launch(
+                Intent(
+                    requireActivity(),
+                    ChangeUserInformationActivity::class.java
+                )
+            )
+        }
     }
 
     private fun initInteractButtons() {
@@ -40,8 +66,9 @@ class MyPageFragment : BaseFragment<FragmentMypageBinding>(
                 requireActivity(),
                 getString(R.string.mypage_logout_confirm),
                 getString(R.string.mypage_logout)
-            ) { startIntentWithRemovingActivityStack(requireActivity(), StartActivity::class.java) }
-
+            ) {
+                startIntentWithRemovingActivityStack(requireActivity(), StartActivity::class.java)
+            }
             logOut()
         }
     }
