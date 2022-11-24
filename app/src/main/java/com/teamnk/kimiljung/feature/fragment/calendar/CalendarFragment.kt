24c0fd252d5ec66020a64cf5d862ca946a5b8f7e
Junsu <krpjs0508@gmail.com>
2022-11-24 22:29:57 +0900

    CalendarFragment

--- app/src/main/java/com/teamnk/kimiljung/feature/fragment/calendar/CalendarFragment.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/fragment/calendar/CalendarFragment.kt
@@ -2,13 +2,7 @@ package com.teamnk.kimiljung.feature.fragment.calendar
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter
 import com.teamnk.kimiljung.R
@@ -20,7 +14,8 @@ class CalendarFragment : BaseFragment<FragmentCalendarBinding>(
     R.layout.fragment_calendar
 ) {
 
-    private val seoul: LatLng by lazy {
+    // TODO remove in future
+    private val latlngSeoul: LatLng by lazy {
         LatLng(37.554891, 126.970814)
     }
 
@@ -28,63 +23,12 @@ class CalendarFragment : BaseFragment<FragmentCalendarBinding>(
         super.onViewCreated(view, savedInstanceState)
 
         initCalendarView()
-        initCreateScheduleImage()
-        initMapLocationDialog(savedInstanceState)
+        initCreateScheduleButton()
     }
 
-    private fun initMapLocationDialog(
-        savedInstanceState: Bundle?,
-    ) {
-        binding.btnCalendarTest.setOnClickListener {
-            BottomSheetDialog(requireActivity()).run {
-                setContentView(
-                    layoutInflater.inflate(R.layout.dialog_map_location, null)
-                )
-                show()
-                findViewById<Button>(R.id.btn_map_delete)?.setOnClickListener {
-                    // TODO 일정 삭제 로직 구현
-                }
-                findViewById<MapView>(R.id.mv_map_location)?.run {
-                    onCreate(savedInstanceState)
-                    it.run {
-                        getMapAsync {
-                            it.run {
-                                addMarker(
-                                    MarkerOptions()
-                                        .title("서울")
-                                        .position(
-                                            LatLng(
-                                                37.554891,
-                                                126.970814,
-                                            )
-                                        )
-                                )
-                                setMinZoomPreference(14F)
-                                setMaxZoomPreference(18F)
-                                moveCamera(
-                                    CameraUpdateFactory.newLatLng(
-                                        LatLng(
-                                            37.554891,
-                                            126.970814,
-                                        )
-                                    )
-                                )
-                                animateCamera(
-                                    CameraUpdateFactory.zoomTo(4F)
-                                )
-                                mapType = GoogleMap.MAP_TYPE_NORMAL
-                            }
-                        }
-                    }
-                    onStart()
-                    onResume()
-                }
-            }
-        }
-    }
 
     private fun initCalendarView() {
-        with(binding.calendarCalendarView) {
+        binding.calendarCalendarView.apply {
             addDecorator(TodayDecorator(requireActivity()))
             setWeekDayFormatter(
                 ArrayWeekDayFormatter(
@@ -106,12 +50,65 @@ class CalendarFragment : BaseFragment<FragmentCalendarBinding>(
         }
     }
 
-    private fun initCreateScheduleImage() {
+    private fun initCreateScheduleButton() {
         binding.imgCalendarScheduleCreate.setOnClickListener {
-            //Todo
+            //TODO When Create Button Clicked
         }
     }
 
     override fun observeEvent() {}
 }
 
+/*
+private fun initMapLocationDialog(
+    savedInstanceState: Bundle?,
+) {
+    binding.btnCalendarTest.setOnClickListener {
+        BottomSheetDialog(requireActivity()).run {
+            setContentView(
+                layoutInflater.inflate(R.layout.dialog_map_location, null)
+            )
+            show()
+            findViewById<Button>(R.id.btn_map_delete)?.setOnClickListener {
+                // TODO 일정 삭제 로직 구현
+            }
+            findViewById<MapView>(R.id.mv_map_location)?.run {
+                onCreate(savedInstanceState)
+                it.run {
+                    getMapAsync {
+                        it.run {
+                            addMarker(
+                                MarkerOptions()
+                                    .title("서울")
+                                    .position(
+                                        LatLng(
+                                            37.554891,
+                                            126.970814,
+                                        )
+                                    )
+                            )
+                            setMinZoomPreference(14F)
+                            setMaxZoomPreference(18F)
+                            moveCamera(
+                                CameraUpdateFactory.newLatLng(
+                                    LatLng(
+                                        37.554891,
+                                        126.970814,
+                                    )
+                                )
+                            )
+                            animateCamera(
+                                CameraUpdateFactory.zoomTo(4F)
+                            )
+                            mapType = GoogleMap.MAP_TYPE_NORMAL
+                        }
+                    }
+                }
+                onStart()
+                onResume()
+            }
+        }
+    }
+}
+
+ */
