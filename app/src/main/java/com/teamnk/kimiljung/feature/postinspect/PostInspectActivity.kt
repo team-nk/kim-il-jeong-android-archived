96a6795b7a2666baee46ecfd6c062e86671c347f
Junsu <krpjs0508@gmail.com>
2022-11-24 23:08:11 +0900

    PostInspectActivity

--- app/src/main/java/com/teamnk/kimiljung/feature/postinspect/PostInspectActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/postinspect/PostInspectActivity.kt
@@ -1,13 +1,11 @@
 package com.teamnk.kimiljung.feature.postinspect
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.teamnk.kimiljung.R
+import com.teamnk.kimiljung.base.BaseActivity
+import com.teamnk.kimiljung.databinding.ActivityPostInspectBinding
 
-class PostInspectActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_inspect)
-        TODO("여기 포스트 이름 다시 지으세요")
-    }
+class PostInspectActivity : BaseActivity<ActivityPostInspectBinding>(
+    R.layout.activity_post_inspect
+) {
+    override fun observeEvent() {}
 }
