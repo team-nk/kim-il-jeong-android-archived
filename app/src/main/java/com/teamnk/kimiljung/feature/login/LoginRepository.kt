b401f7318081b417f8832a7be20cc5494d7fa34c
Junsu <krpjs0508@gmail.com>
2022-11-24 22:13:17 +0900

    apiProvider

--- app/src/main/java/com/teamnk/kimiljung/feature/login/LoginRepository.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/login/LoginRepository.kt
@@ -1,9 +1,9 @@
 package com.teamnk.kimiljung.feature.login
 
-import com.teamnk.kimiljung.api.loginAPI
+import com.teamnk.kimiljung.api.loginAPIProvider
 import retrofit2.Response
 
 class LoginRepository {
     suspend fun login(loginRequest: LoginRequest): Response<LoginResponse> =
-        loginAPI.login(loginRequest)
+        loginAPIProvider.login(loginRequest)
 }
