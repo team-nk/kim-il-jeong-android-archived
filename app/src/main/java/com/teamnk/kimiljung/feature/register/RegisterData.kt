e2eb49ffbf56d2fb91f6ad123ffa8965d3d17589
Junsu <krpjs0508@gmail.com>
2022-11-28 22:48:50 +0900

    feat :: registerData

--- app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterData.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterData.kt
@@ -5,3 +5,11 @@ import com.google.gson.annotations.SerializedName
 data class CheckIdDuplicationRequest(
     @SerializedName("accountId") val accountId: String,
 )
+
+data class RegisterRequest(
+    @SerializedName("email") val email: String,
+    @SerializedName("code") val code: String,
+    @SerializedName("accountId") val accountId: String,
+    @SerializedName("password") val password: String,
+    @SerializedName("repassword") val repeatPassword: String,
+)
