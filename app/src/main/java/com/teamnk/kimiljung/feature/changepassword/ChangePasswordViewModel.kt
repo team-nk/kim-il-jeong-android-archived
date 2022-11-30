bd5fbf36e38258e147b239415f3f8616e27cf3e2
Junsu <krpjs0508@gmail.com>
2022-11-30 21:34:06 +0900

    feat :: implement ChangePasswordViewModel

--- app/src/main/java/com/teamnk/kimiljung/feature/changepassword/ChangePasswordViewModel.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/changepassword/ChangePasswordViewModel.kt
@@ -1,7 +1,42 @@
 package com.teamnk.kimiljung.feature.changepassword
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
 class ChangePasswordViewModel(
     private val repository: ChangePasswordRepository
-) {
+) : ViewModel() {
 
+    private val tag = this.javaClass.simpleName
+
+    private val _isPasswordChangeSuccess = MutableLiveData<Boolean>()
+    val isPasswordChangeSuccess: LiveData<Boolean> = _isPasswordChangeSuccess
+
+    fun changePassword(
+        changePasswordRequest: ChangePasswordRequest
+    ) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                repository.changePassword(
+                    changePasswordRequest = changePasswordRequest
+                )
+            }.onSuccess {
+                if (it.isSuccessful) {
+                    _isPasswordChangeSuccess.postValue(
+                        true
+                    )
+                    Log.d(tag, "changePassword success!")
+                } else {
+                    _isPasswordChangeSuccess.postValue(
+                        false
+                    )
+                    Log.d(tag, "changePassword failure..")
+                }
+            }
+        }
+    }
 }
