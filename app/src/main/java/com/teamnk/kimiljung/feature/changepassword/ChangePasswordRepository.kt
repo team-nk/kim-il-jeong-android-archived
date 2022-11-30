491ae5df4003b97cd513eaa4d34a6f7cc0d7ac60
Junsu <krpjs0508@gmail.com>
2022-11-30 21:33:17 +0900

    feat :: implement changePassword logic in ChangePasswordRepository

--- app/src/main/java/com/teamnk/kimiljung/feature/changepassword/ChangePasswordRepository.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/changepassword/ChangePasswordRepository.kt
@@ -1,4 +1,15 @@
 package com.teamnk.kimiljung.feature.changepassword
 
+import com.teamnk.kimiljung.api.changePasswordAPIProvider
+import retrofit2.Response
+
 class ChangePasswordRepository {
+
+    suspend fun changePassword(
+        changePasswordRequest: ChangePasswordRequest
+    ): Response<Void> {
+        return changePasswordAPIProvider.changePassword(
+            changePasswordRequest = changePasswordRequest
+        )
+    }
 }
