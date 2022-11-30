e68eeb0f7851a0e76c5b34b82d745c5125a96689
Junsu <krpjs0508@gmail.com>
2022-11-30 21:48:07 +0900

    feat :: implement ChangePasswordActivity logic

--- app/src/main/java/com/teamnk/kimiljung/feature/changepassword/ChangePasswordActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/changepassword/ChangePasswordActivity.kt
@@ -1,11 +1,12 @@
 package com.teamnk.kimiljung.feature.changepassword
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.base.BaseActivity
 import com.teamnk.kimiljung.databinding.ActivityChangePasswordBinding
-import com.teamnk.kimiljung.feature.login.LoginViewModel
+import com.teamnk.kimiljung.util.showShortSnackBar
 
 class ChangePasswordActivity : BaseActivity<ActivityChangePasswordBinding>(
     R.layout.activity_change_password
@@ -13,8 +14,10 @@ class ChangePasswordActivity : BaseActivity<ActivityChangePasswordBinding>(
 
     private val viewModel by lazy {
         ViewModelProvider(
-            this, ChangePasswordViewModelFactory(ChangePasswordRepository())
-        )[LoginViewModel::class.java]
+            this, ChangePasswordViewModelFactory(
+                ChangePasswordRepository()
+            )
+        )[ChangePasswordViewModel::class.java]
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +33,13 @@ class ChangePasswordActivity : BaseActivity<ActivityChangePasswordBinding>(
 
     private fun initChangeButton() {
         binding.btnActivityChangePasswordChange.setOnClickListener {
-
+            viewModel.changePassword(
+                ChangePasswordRequest(
+                    oldPassword = binding.etActivityChangePasswordOldPassword.text.toString(),
+                    newPassword = binding.etActivityChangePasswordNewPassword.text.toString(),
+                    newPasswordRepeat = binding.etActivityChangePasswordNewPasswordRepeat.text.toString(),
+                )
+            )
         }
     }
 
@@ -41,6 +50,25 @@ class ChangePasswordActivity : BaseActivity<ActivityChangePasswordBinding>(
     }
 
     override fun observeEvent() {
-
+        viewModel.isPasswordChangeSuccess.observe(
+            this
+        ) {
+            if (it) {
+                setResult(
+                    RESULT_OK,
+                    Intent().putExtra(
+                        "isChangePasswordSuccess",
+                        true,
+                    ),
+                ).also {
+                    finish()
+                }
+            } else {
+                showShortSnackBar(
+                    view = binding.root,
+                    text = getString(R.string.error_failed_to_connect_to_server),
+                )
+            }
+        }
     }
 }
