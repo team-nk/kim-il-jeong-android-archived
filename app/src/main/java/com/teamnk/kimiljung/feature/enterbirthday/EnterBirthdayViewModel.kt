f37c03a78c9a9444b678bfbfa5533e5382aca017
Junsu <krpjs0508@gmail.com>
2022-12-02 19:49:58 +0900

    feat :: implement EnterbirthdayViewModel

---
+++ app/src/main/java/com/teamnk/kimiljung/feature/enterbirthday/EnterBirthdayViewModel.kt
@@ -0,0 +1,9 @@
+package com.teamnk.kimiljung.feature.enterbirthday
+
+import androidx.lifecycle.ViewModel
+
+class EnterBirthdayViewModel(
+    private val repository: EnterBirthdayRepository
+) : ViewModel() {
+
+}
