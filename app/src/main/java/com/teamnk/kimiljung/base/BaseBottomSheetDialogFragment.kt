40b19ca685a4a2f2c944f08b6269f154423e74e6
Junsu <krpjs0508@gmail.com>
2022-12-02 19:50:36 +0900

    feat :: implement BaseBottomSheetDialogFragment

---
+++ app/src/main/java/com/teamnk/kimiljung/base/BaseBottomSheetDialogFragment.kt
@@ -0,0 +1,38 @@
+package com.teamnk.kimiljung.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+abstract class BaseBottomSheetDialogFragment<B : ViewDataBinding>(
+    @LayoutRes private val layoutID: Int,
+) : BottomSheetDialogFragment() {
+
+    protected lateinit var binding: B
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            layoutID,
+            container,
+            false,
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = this
+        observeEvent()
+    }
+
+    abstract fun observeEvent()
+}
