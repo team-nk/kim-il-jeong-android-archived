56e84ff6d21fd5dbb075666f79bf4f83c1ed2938
Junsu <krpjs0508@gmail.com>
2022-12-05 17:06:25 +0900

    feat :: implement EnterBirthdayViewModel

--- app/src/main/java/com/teamnk/kimiljung/feature/enterbirthday/EnterBirthdayViewModel.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/enterbirthday/EnterBirthdayViewModel.kt
@@ -1,9 +1,33 @@
 package com.teamnk.kimiljung.feature.enterbirthday
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class EnterBirthdayViewModel(
     private val repository: EnterBirthdayRepository
 ) : ViewModel() {
 
+    private val _isEnterBirthdaySuccess = MutableLiveData<Boolean>()
+    val isEnterBirthdaySuccess: LiveData<Boolean>
+        get() = _isEnterBirthdaySuccess
+
+    val selectedDate = MutableLiveData<String>()
+
+    fun enterBirthday(
+        enterBirthdayRequest: EnterBirthdayRequest
+    ) {
+        viewModelScope.launch(Dispatchers.IO) {
+            kotlin.runCatching {
+                repository.enterBirthday(enterBirthdayRequest)
+            }.onSuccess {
+                _isEnterBirthdaySuccess.postValue(
+                    it.isSuccessful
+                )
+            }
+        }
+    }
 }
