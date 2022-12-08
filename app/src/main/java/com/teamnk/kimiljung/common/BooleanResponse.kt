de4ca74d55276aa992f2ff7e39b045c0422be23a
Junsu <krpjs0508@gmail.com>
2022-12-08 20:30:18 +0900

    refactor :: rename True -> BooleanResponse

--- app/src/main/java/com/teamnk/kimiljung/common/BooleanResponse.kt
+++ app/src/main/java/com/teamnk/kimiljung/common/BooleanResponse.kt
@@ -2,6 +2,6 @@ package com.teamnk.kimiljung.common
 
 import com.google.gson.annotations.SerializedName
 
-data class True(
+data class BooleanResponse(
     @SerializedName("true") val boolean: Boolean,
 )
