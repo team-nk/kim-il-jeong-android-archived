58cb80c9902d1ee56ee1ba374372eca9d4f3a802
Junsu <krpjs0508@gmail.com>
2022-12-08 20:08:34 +0900

    feat :: VerifyEmailRequest

--- app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterData.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterData.kt
@@ -2,6 +2,10 @@ package com.teamnk.kimiljung.feature.register
 
 import com.google.gson.annotations.SerializedName
 
+data class VerifyEmailRequest(
+    @SerializedName("email") val email: String,
+)
+
 data class CheckIdDuplicationRequest(
     @SerializedName("accountId") val accountId: String,
 )
