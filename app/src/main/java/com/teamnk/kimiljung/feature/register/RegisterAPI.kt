f77936be438c8374f4547d91dcc15c70f4ab12eb
Junsu <krpjs0508@gmail.com>
2022-12-08 20:09:14 +0900

    feat :: implement RegisterAPI verifyEmail method

--- app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterAPI.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterAPI.kt
@@ -7,13 +7,19 @@ import retrofit2.http.POST
 
 interface RegisterAPI {
 
+    // TODO
+    @GET("/user")
+    suspend fun verifyEmail(
+        @Body verifyEmailRequest: VerifyEmailRequest,
+    ): Response<Boolean>
+
     @GET("/user/check")
     suspend fun checkIdDuplication(
-        @Body checkIdDuplicationRequest: CheckIdDuplicationRequest
+        @Body checkIdDuplicationRequest: CheckIdDuplicationRequest,
     ): Response<Boolean>
 
     @POST("/user")
     suspend fun register(
-        @Body registerRequest: RegisterRequest
+        @Body registerRequest: RegisterRequest,
     ): Response<Void>
 }
