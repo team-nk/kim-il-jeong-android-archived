37b8a6f509831e7b34eed61ec144eec825b0a19e
Junsu <krpjs0508@gmail.com>
2022-12-08 20:01:09 +0900

    refactor :: make viewmodel as a AndroidViewModel

--- app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterViewModel.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterViewModel.kt
@@ -1,24 +1,37 @@
 package com.teamnk.kimiljung.feature.register
 
+import android.app.Application
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.teamnk.kimiljung.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class RegisterViewModel(
-    private val repository: RegisterRepository
-) : ViewModel() {
+    private val repository: RegisterRepository,
+    private val mApplication: Application,
+) : AndroidViewModel(mApplication) {
 
     private val tag = this.javaClass.simpleName
+
+    private val _shouldShowSnackBar = MutableLiveData<Pair<Boolean, String>>()
+    val shouldShowSnackBar: LiveData<Pair<Boolean, String>>
+        get() = _shouldShowSnackBar
 
     private val _checkIdDuplicationResponse = MutableLiveData<Boolean>()
     val checkIdDuplicationResponse: LiveData<Boolean> = _checkIdDuplicationResponse
 
     private val _registerResponse = MutableLiveData<Boolean>()
     val registerResponse: LiveData<Boolean> = _registerResponse
+
+    var isEmailVerified = false
+
+    var isVerificationCodeChecked = false
+
+    var isIdDuplicationChecked = false
 
     fun checkIdDuplication(
         checkIdDuplicationRequest: CheckIdDuplicationRequest,
@@ -31,6 +44,7 @@ class RegisterViewModel(
             }.onSuccess {
                 if (it.isSuccessful) {
                     _checkIdDuplicationResponse.postValue(it.body())
+                    isIdDuplicationChecked = true
                     Log.d(tag, "checkIdDuplication success!")
                 } else {
                     Log.e(tag, "checkIdDuplication failure..")
@@ -42,24 +56,35 @@ class RegisterViewModel(
     fun register(
         registerRequest: RegisterRequest
     ) {
-        viewModelScope.launch(Dispatchers.IO) {
-            kotlin.runCatching {
-                repository.register(
-                    registerRequest
-                )
-            }.onSuccess {
-                if (it.isSuccessful) {
-                    _registerResponse.postValue(
-                        it.isSuccessful
+        if (isEmailVerified && isVerificationCodeChecked && isIdDuplicationChecked) {
+            viewModelScope.launch(Dispatchers.IO) {
+                kotlin.runCatching {
+                    repository.register(
+                        registerRequest
                     )
-                    Log.d(tag, "register success!")
-                } else {
-                    _registerResponse.postValue(
-                        false
-                    )
-                    Log.e(tag, "register failure..")
+                }.onSuccess {
+                    if (it.isSuccessful) {
+                        _registerResponse.postValue(
+                            it.isSuccessful
+                        )
+                        Log.d(tag, "register success!")
+                    } else {
+                        _registerResponse.postValue(
+                            false
+                        )
+                        Log.e(tag, "register failure..")
+                    }
                 }
             }
+        } else {
+            _shouldShowSnackBar.postValue(
+                Pair(
+                    true,
+                    mApplication.getString(
+                        R.string.error_activity_register_please_enter_necessary_information,
+                    ),
+                )
+            )
         }
     }
 }
