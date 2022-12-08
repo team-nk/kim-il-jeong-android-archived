c886d0e67a14ac330cdc497fa0c2347649de880f
Junsu <krpjs0508@gmail.com>
2022-12-08 20:09:30 +0900

    feat :: implement verifyEmailRequest in RegisterRepository

--- app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterRepository.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterRepository.kt
@@ -3,15 +3,19 @@ package com.teamnk.kimiljung.feature.register
 import com.teamnk.kimiljung.api.registerAPIProvider
 import retrofit2.Response
 
-class RegisterRepository {
+class RegisterRepository : RegisterAPI {
 
-    suspend fun checkIdDuplication(
+    override suspend fun verifyEmail(verifyEmailRequest: VerifyEmailRequest): Response<Boolean> {
+        return registerAPIProvider.verifyEmail(verifyEmailRequest)
+    }
+
+    override suspend fun checkIdDuplication(
         checkIdDuplicationRequest: CheckIdDuplicationRequest,
     ): Response<Boolean> {
         return registerAPIProvider.checkIdDuplication(checkIdDuplicationRequest)
     }
 
-    suspend fun register(
+    override suspend fun register(
         registerRequest: RegisterRequest
     ): Response<Void> {
         return registerAPIProvider.register(registerRequest)
