0c2f2449cc73d94ec518b2098074c8f7cc76dbaa
Tmdhoon2 <hks026naver@gmail.com>
2022-12-26 04:58:50 +0900

    feat :: implement check user permission logic in map fragment

--- app/src/main/java/com/teamnk/kimiljung/feature/map/MapFragment.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/map/MapFragment.kt
@@ -1,15 +1,23 @@
 package com.teamnk.kimiljung.feature.map
 
+import android.Manifest
 import android.content.Context.LOCATION_SERVICE
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.View
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.teamnk.kimiljung.BuildConfig
 import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.base.BaseFragment
 import com.teamnk.kimiljung.databinding.FragmentMapBinding
+import com.teamnk.kimiljung.util.showShortToast
 
 class MapFragment : BaseFragment<FragmentMapBinding>(
     R.layout.fragment_map,
@@ -27,14 +35,22 @@ class MapFragment : BaseFragment<FragmentMapBinding>(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        checkUserPermission()
         setUserLocation()
         initMapView()
+
+    }
+
+    private fun checkUserPermission(){
+        if(requireActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            showShortToast(requireActivity(), getString(R.string.activity_introduction_pager_accept_permission))
+            startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + BuildConfig.APPLICATION_ID)))
+        }
     }
 
     private fun initMapView(){
         childFragmentManager.beginTransaction()
             .replace(R.id.map_fragment_map_main, mapFragment, "MapTag").commit()
-        mapFragment.getMapAsync(this@MapFragment)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -58,6 +74,7 @@ class MapFragment : BaseFragment<FragmentMapBinding>(
     private fun setUserLocation(){
         locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)?.run {
             currentLocation = LatLng(this.latitude, this.longitude)
+            mapFragment.getMapAsync(this@MapFragment)
         }
     }
 
