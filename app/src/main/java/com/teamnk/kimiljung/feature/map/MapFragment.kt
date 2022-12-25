02208f746cbf2779d1d7219fec4edc87ecfc7987
Tmdhoon2 <hks026naver@gmail.com>
2022-12-26 04:42:38 +0900

    fix :: map logic

--- app/src/main/java/com/teamnk/kimiljung/feature/map/MapFragment.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/map/MapFragment.kt
@@ -1,12 +1,9 @@
-package com.teamnk.kimiljung.feature.fragment.map
+package com.teamnk.kimiljung.feature.map
 
-import android.Manifest
 import android.content.Context.LOCATION_SERVICE
-import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Bundle
 import android.view.View
-import androidx.core.app.ActivityCompat
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -22,27 +19,25 @@ class MapFragment : BaseFragment<FragmentMapBinding>(
         SupportMapFragment.newInstance()
     }
 
-    private val mapView: MapView by lazy {
-        binding.mapFragmentMapMain
-    }
-
     private val locationManager: LocationManager by lazy {
         requireActivity().getSystemService(LOCATION_SERVICE) as LocationManager
     }
 
-    private lateinit var currentLocation : LatLng
+    private lateinit var currentLocation: LatLng
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mapView.run {
-            this.onCreate(savedInstanceState)
-            childFragmentManager.beginTransaction().replace(R.id.map_fragment_map_main, mapFragment, "MapTag").commit()
-            mapFragment.getMapAsync(this@MapFragment)
-        }
+        setUserLocation()
+        initMapView()
+    }
+
+    private fun initMapView(){
+        childFragmentManager.beginTransaction()
+            .replace(R.id.map_fragment_map_main, mapFragment, "MapTag").commit()
+        mapFragment.getMapAsync(this@MapFragment)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        checkPermission()
         googleMap.run {
             addMarker(
                 MarkerOptions()
@@ -60,19 +55,11 @@ class MapFragment : BaseFragment<FragmentMapBinding>(
         }
     }
 
-    override fun observeEvent() {}
-
-    private fun checkPermission() {
-        if (requireActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                10,
-            )
-        } else {
-            locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)?.apply {
-                currentLocation = LatLng(this.latitude, this.longitude)
-            }
+    private fun setUserLocation(){
+        locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)?.run {
+            currentLocation = LatLng(this.latitude, this.longitude)
         }
     }
+
+    override fun observeEvent() {}
 }
