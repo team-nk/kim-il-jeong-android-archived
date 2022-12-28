8f996cf664c8cf45c03c2673ed7694102ee1da8d
JunJaBoy <krpjs0508@gmail.com>
2022-12-28 23:38:40 +0900

    feat :: RetrofitClient GsonBuilder 추가

--- app/src/main/java/com/teamnk/kimiljung/api/RetrofitClient.kt
+++ app/src/main/java/com/teamnk/kimiljung/api/RetrofitClient.kt
@@ -1,5 +1,6 @@
 package com.teamnk.kimiljung.api
 
+import com.google.gson.GsonBuilder
 import com.teamnk.kimiljung.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,9 +11,10 @@ object RetrofitClient {
     private var retrofit: Retrofit? = null
 
     fun getRetrofit(): Retrofit? {
-        return retrofit ?: Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        return retrofit ?: Retrofit.Builder().baseUrl(BuildConfig.BASE_URL).addConverterFactory(
+            GsonConverterFactory.create(
+                GsonBuilder().setLenient().create(),
+            )
+        ).build()
     }
 }
