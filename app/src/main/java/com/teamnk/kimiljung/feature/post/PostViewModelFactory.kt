9b846ebe1ecb00fefebb4cfbb9f73a7f6e913ffb
Tmdhoon2 <hks026naver@gmail.com>
2022-12-28 22:01:19 +0900

    refactor :: application

--- app/src/main/java/com/teamnk/kimiljung/feature/post/PostViewModelFactory.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/post/PostViewModelFactory.kt
@@ -1,15 +1,18 @@
 package com.teamnk.kimiljung.feature.post
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 @Suppress("UNCHECKED_CAST")
 class PostViewModelFactory(
-    private val postRepository : PostRepository
+    private val postRepository : PostRepository,
+    private val application : Application,
 ) : ViewModelProvider.Factory{
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T =
         PostViewModel(
             postRepository = postRepository,
+            application = application,
         ) as T
 }
