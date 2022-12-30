7f55ed3f8d504f56af99e1eb177f7b7b48f2aaee
JunJaBoy <krpjs0508@gmail.com>
2022-12-30 23:32:52 +0900

    refactor :: RegisterData 리퀘스트 변수명 수정

--- app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterData.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/register/RegisterData.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class RegisterRequest(
     @SerializedName("email") val email: String,
     @SerializedName("code") val verificationCode: String,
-    @SerializedName("accountId") val accountId: String,
+    @SerializedName("account_id") val accountId: String,
     @SerializedName("password") val password: String,
-    @SerializedName("repassword") val repeatPassword: String,
+    @SerializedName("re_password") val repeatPassword: String,
 )
