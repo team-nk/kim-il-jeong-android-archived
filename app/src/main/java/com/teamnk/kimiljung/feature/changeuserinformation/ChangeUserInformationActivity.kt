e7a13ee0fe820a8cde6d8f2cf94cd909748025a5
junsuPark <krpjs0508@gmail.com>
2022-12-31 23:03:18 +0900

    feat :: ChangeUserInformationActivity 구현

--- app/src/main/java/com/teamnk/kimiljung/feature/changeuserinformation/ChangeUserInformationActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/changeuserinformation/ChangeUserInformationActivity.kt
@@ -1,11 +1,44 @@
 package com.teamnk.kimiljung.feature.changeuserinformation
 
+import androidx.activity.viewModels
 import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.base.BaseActivity
 import com.teamnk.kimiljung.databinding.ActivityChangeUserInformationBinding
+import com.teamnk.kimiljung.util.loadImage
 
 class ChangeUserInformationActivity : BaseActivity<ActivityChangeUserInformationBinding>(
     R.layout.activity_change_user_information,
 ) {
+
+    private val viewModel by viewModels<ChangeUserInformationViewModel>()
+
     override fun observeEvent() {}
+
+    private fun initView() {
+        with(binding) {
+            intent.getStringExtra(
+                PROFILE_URL,
+            )?.let {
+                imageActivityChangeUserInformationUserProfile.loadImage(
+                    it
+                )
+            }
+
+            intent.getStringExtra(
+                EMAIL,
+            )?.let {
+                etActivityChangeUserInformationEmail.hint = it
+            }
+
+            intent.getStringExtra(
+                ID,
+            )?.let {
+                etActivityChangeUserInformationId.hint = it
+            }
+        }
+    }
 }
+
+const val PROFILE_URL = "profile_url"
+const val EMAIL = "email"
+const val ID = "id"
