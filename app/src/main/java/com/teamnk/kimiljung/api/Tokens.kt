6634a6fe2f4fb7a0fcbb7a844357aa0c2a22ff5a
junsuPark <krpjs0508@gmail.com>
2022-12-31 15:46:51 +0900

    feat :: TokenAPI 구현

--- app/src/main/java/com/teamnk/kimiljung/api/Tokens.kt
+++ app/src/main/java/com/teamnk/kimiljung/api/Tokens.kt
@@ -2,6 +2,13 @@ package com.teamnk.kimiljung.api
 
 var accessToken: String? = null
 var refreshToken: String? = nulldata class TokenResponse(
+
+interface TokenAPI {
+
+    @PUT("/auth")
+    fun renewToken(): Response<TokenResponse>
+}
+
 data class TokenResponse(
     @SerializedName("access_token") val accessToken: String,
     @SerializedName("refresh_token") val refreshToken: String,
