55beb92a2a3ba90e646f2dd97a30fb5fa28989d0
junsuPark <krpjs0508@gmail.com>
2022-12-31 10:53:20 +0900

    feat :: StartActivityUtil startActivity 기본 함수 구현
    정승훈 화이팅

--- app/src/main/java/com/teamnk/kimiljung/util/StartActivityUtil.kt
+++ app/src/main/java/com/teamnk/kimiljung/util/StartActivityUtil.kt
@@ -5,6 +5,20 @@ import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 
+internal fun <T : Activity> Activity.startActivity(
+    context: Context,
+    to: Class<out T>,
+    flag: Int = FLAG_ACTIVITY_CLEAR_TOP,
+) {
+    startActivity(
+        Intent(
+            /* packageContext = */
+            context,
+            /* cls = */
+            to,
+        )
+    )
+}
 fun <T> startActivity(context: Context, to: Class<T>) {
     context.startActivity(Intent(context, to))
 }
