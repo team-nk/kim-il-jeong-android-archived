d72c6a27342a955fd87030fa0bfcdf59eb690884
Tmdhoon2 <hks026naver@gmail.com>
2022-12-31 15:31:49 +0900

    feat :: null check 옵션 추가

--- app/src/main/java/com/teamnk/kimiljung/feature/postcomment/PostCommentActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/postcomment/PostCommentActivity.kt
@@ -1,16 +1,19 @@
 package com.teamnk.kimiljung.feature.postcomment
 
+import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.teamnk.kimiljung.R
 import com.teamnk.kimiljung.base.BaseActivity
 import com.teamnk.kimiljung.databinding.ActivityPostCommentBinding
+import com.teamnk.kimiljung.util.showShortSnackBar
 
 class PostCommentActivity : BaseActivity<ActivityPostCommentBinding>(
     R.layout.activity_post_comment
 ) {
 
-    private val commentViewModel by lazy {
+    private val viewModel by lazy {
         ViewModelProvider(
             this,
             CommentViewModelFactory(
@@ -20,18 +23,52 @@ class PostCommentActivity : BaseActivity<ActivityPostCommentBinding>(
         )[CommentViewModel::class.java]
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initSendButton()
+    }
+
+    private fun initSendButton() {
+        binding.run {
+            btnActivityPostCommentSend.setOnClickListener{
+                etActivityPostCommentComment.text.toString().run {
+                    if(this.isNotBlank()){
+                        viewModel.postComment(
+                            postId = intent.getIntExtra("id", 0),
+                            commentRequest = CommentRequest(etActivityPostCommentComment.text.toString())
+                        )
+                    }else{
+                        showShortSnackBar(
+                            view = binding.root,
+                            text = this@PostCommentActivity.getString(R.string.activity_post_comment_please_enter_comment)
+                        )
+                    }
+                }
+            }
+        }
+    }
+
     override fun observeEvent() {
-        commentViewModel.commentListResponse.observe(
+        viewModel.commentListResponse.observe(
             this,
-        ){
+        ) {
             initCommentListRecyclerView(
                 commentList = it.body()!!.comment_list,
+            )
+        }
+
+        viewModel.snackBarMessage.observe(
+            this,
+        ){
+            showShortSnackBar(
+                view = binding.root,
+                text = it
             )
         }
     }
 
     private fun initCommentListRecyclerView(
-        commentList : ArrayList<CommentList>,
+        commentList: ArrayList<CommentList>,
     ) {
         binding.rvActivityPostCommentMain.apply {
             adapter = PostCommentAdapter(
