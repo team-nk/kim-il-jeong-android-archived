8397a32b8d4d6a3cc8adfcbb81dc44bc80bf3535
junsuPark <krpjs0508@gmail.com>
2022-12-31 17:12:39 +0900

    refactor :: trailing comma

--- app/src/main/java/com/teamnk/kimiljung/feature/changeuserinformation/ChangeUserInformationActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/changeuserinformation/ChangeUserInformationActivity.kt
@@ -5,7 +5,7 @@ import com.teamnk.kimiljung.base.BaseActivity
 import com.teamnk.kimiljung.databinding.ActivityChangeUserInformationBinding
 
 class ChangeUserInformationActivity : BaseActivity<ActivityChangeUserInformationBinding>(
-    R.layout.activity_change_user_information
+    R.layout.activity_change_user_information,
 ) {
     override fun observeEvent() {}
 }
