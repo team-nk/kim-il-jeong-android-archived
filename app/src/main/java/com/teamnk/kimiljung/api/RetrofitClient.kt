68ac2a03d2c77460a387221912965252f5fb20b6
junsuPark <krpjs0508@gmail.com>
2022-12-31 15:50:54 +0900

    RetrofitClient 구현

--- app/src/main/java/com/teamnk/kimiljung/api/RetrofitClient.kt
+++ app/src/main/java/com/teamnk/kimiljung/api/RetrofitClient.kt
@@ -43,4 +43,19 @@ object ResponseInterceptor : Interceptor {
         }
     }
 }
+
+object ResponseCodes {
+
+    const val OK = 200
+    const val CREATED = 201
+    const val ACCEPTED = 202
+    const val NO_CONTENT = 204
+
+    const val BAD_REQUEST = 400
+    const val UNAUTHORIZED = 401
+    const val FORBIDDEN = 403
+    const val NOT_FOUND = 404
+    const val METHOD_NOT_ALLOWED = 405
+    const val CONFLICT = 409
+    const val TOO_MANY_REQUESTS = 429
 }
