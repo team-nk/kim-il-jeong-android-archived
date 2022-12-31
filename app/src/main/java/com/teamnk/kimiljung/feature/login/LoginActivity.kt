8148b0b73674a3dd88b47c4179bd2416a6a118f1
junsuPark <krpjs0508@gmail.com>
2022-12-31 11:22:25 +0900

    refactor :: LoginActivity 레거시 startActivity 코드 제거

--- app/src/main/java/com/teamnk/kimiljung/feature/login/LoginActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/login/LoginActivity.kt
@@ -11,7 +11,7 @@ import com.teamnk.kimiljung.util.SharedPreferencesKey.ACCESS_TOKEN
 import com.teamnk.kimiljung.util.SharedPreferencesKey.IS_LOGGED_IN
 import com.teamnk.kimiljung.util.SharedPreferencesKey.REFRESH_TOKEN
 import com.teamnk.kimiljung.util.showShortSnackBar
-import com.teamnk.kimiljung.util.startActivityLegacy
+import com.teamnk.kimiljung.util.startActivity
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(
     R.layout.activity_login
@@ -68,7 +68,7 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(
 
     private fun moveToMainActivity() {
         defaultSharedPreferencesEditor.putBoolean(IS_LOGGED_IN, true).apply()
-        startActivityLegacy(
+        startActivity(
             context = this,
             to = MainActivity::class.java,
         )
@@ -77,7 +77,7 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(
 
     private fun initGoToRegisterText() {
         binding.tvActivityLoginGoToRegister.setOnClickListener {
-            startActivityLegacy(
+            startActivity(
                 context = this,
                 to = RegisterActivity::class.java,
             )
