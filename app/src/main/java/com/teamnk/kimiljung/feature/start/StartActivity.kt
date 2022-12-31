9b954f1d9d096c2a988c8ca34418abd18241dafb
junsuPark <krpjs0508@gmail.com>
2022-12-31 11:30:54 +0900

    feat :: StartActivity startActivity 로직들들 변경

--- app/src/main/java/com/teamnk/kimiljung/feature/start/StartActivity.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/start/StartActivity.kt
@@ -7,7 +7,7 @@ import com.teamnk.kimiljung.databinding.ActivityStartBinding
 import com.teamnk.kimiljung.feature.introduction.IntroductionPagerActivity
 import com.teamnk.kimiljung.feature.login.LoginActivity
 import com.teamnk.kimiljung.util.SharedPreferencesKey.IS_INTRODUCTION_PAGER_SHOWN
-import com.teamnk.kimiljung.util.startActivityLegacy
+import com.teamnk.kimiljung.util.startActivity
 
 class StartActivity : BaseActivity<ActivityStartBinding>(
     R.layout.activity_start
@@ -25,7 +25,7 @@ class StartActivity : BaseActivity<ActivityStartBinding>(
                 IS_INTRODUCTION_PAGER_SHOWN, false
             ).not()
         ) {
-            startActivityLegacy(
+            startActivity(
                 context = this,
                 to = IntroductionPagerActivity::class.java,
             )
@@ -34,7 +34,7 @@ class StartActivity : BaseActivity<ActivityStartBinding>(
 
     private fun initLoginEmailButton() {
         binding.tvActivityStartLoginEmail.setOnClickListener {
-            startActivityLegacy(
+            startActivity(
                 context = this,
                 to = LoginActivity::class.java,
             )
