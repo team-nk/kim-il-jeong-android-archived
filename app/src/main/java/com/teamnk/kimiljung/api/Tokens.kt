df6448574b8fda951e035f436607b0e459611694
junsuPark <krpjs0508@gmail.com>
2022-12-31 15:46:33 +0900

    feat :: TokenResponse 구현 2

--- app/src/main/java/com/teamnk/kimiljung/api/Tokens.kt
+++ app/src/main/java/com/teamnk/kimiljung/api/Tokens.kt
@@ -2,6 +2,7 @@ package com.teamnk.kimiljung.api
 
 var accessToken: String? = null
 var refreshToken: String? = nulldata class TokenResponse(
+data class TokenResponse(
     @SerializedName("access_token") val accessToken: String,
     @SerializedName("refresh_token") val refreshToken: String,
 )
