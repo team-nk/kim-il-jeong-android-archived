7b01bd6f3234cb1b26e36317a2b259dd78bb49d8
junsuPark <krpjs0508@gmail.com>
2023-01-01 18:40:44 +0900

    feat :: snackBarMessage 표시

--- app/src/main/java/com/teamnk/kimiljung/feature/enterbirthday/EnterBirthdayViewModel.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/enterbirthday/EnterBirthdayViewModel.kt
@@ -1,21 +1,28 @@
 package com.teamnk.kimiljung.feature.enterbirthday
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.teamnk.kimiljung.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class EnterBirthdayViewModel(
-    private val repository: EnterBirthdayRepository
-) : ViewModel() {
+    private val repository: EnterBirthdayRepository,
+    private val mApplication: Application,
+) : AndroidViewModel(mApplication) {
 
     private val _isEnterBirthdaySuccess = MutableLiveData<Boolean>()
     val isEnterBirthdaySuccess: LiveData<Boolean>
         get() = _isEnterBirthdaySuccess
 
-    private var selectedBirthday: String? = String()
+    private val _snackBarMessage = MutableLiveData<String>()
+    val snackBarMessage : LiveData<String>
+    get() = _snackBarMessage
+
+    private lateinit var selectedBirthday: String
 
     fun setBirthday(birthday: String) {
         selectedBirthday = birthday
