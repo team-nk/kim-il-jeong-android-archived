cda352b9e6244e5659988ac9da7daec12efabef3
junsuPark <krpjs0508@gmail.com>
2023-01-05 19:16:59 +0900

    feat :: ChangeUserInformationAPI 구현

--- app/src/main/java/com/teamnk/kimiljung/feature/changeuserinformation/ChangeUserInformationAPI.kt
+++ app/src/main/java/com/teamnk/kimiljung/feature/changeuserinformation/ChangeUserInformationAPI.kt
@@ -1,6 +1,8 @@
 package com.teamnk.kimiljung.feature.changeuserinformation
 
+import retrofit2.Response
+
 interface ChangeUserInformationAPI {
 
-
+    suspend fun changeUserInformation(changeUserInformationRequest: ChangeUserInformationRequest): Response<Void>
 }
